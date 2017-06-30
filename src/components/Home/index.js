73d806726813a904b835c3ede10e453b855b0246
Frankie Simms <fdsimms@gmail.com>
2017-06-30 10:33:35 -0400

    wip

--- src/components/Home/index.js
+++ src/components/Home/index.js
@@ -2,6 +2,13 @@ import React from "react";
 
 import styles from "./Home.css";
 
-const Home = () => <div />;
+const Home = () =>
+  <div>
+    <div>logo</div>
+    <p>
+      A collection of 16,340,976 free digital artifacts from
+      over 120 libraries, archives and museums
+    </p>
+  </div>;
 
 export default Home;
