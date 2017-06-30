e25a2ad66b6699554d5e4db10cc3701432869f71
Frankie Simms <fdsimms@gmail.com>
2017-06-30 17:14:22 -0400

    wip

--- src/components/Home/modules/OnlineExhibitions/index.js
+++ src/components/Home/modules/OnlineExhibitions/index.js
@@ -1,11 +1,11 @@
 import React from "react";
-import Carousel from "nuka-carousel";
+import Slider from "react-slick";
 
 import styles from "./OnlineExhibitions.css";
 
 const mockExhibitionsData = [
   {
-    text: "In Focus: The Evolution of the Personal Camera",
+    text: "Building the First Transcontinental Railroad",
     imageUrl: "http://via.placeholder.com/334x243",
     id: 0,
     isFeatured: true
@@ -30,30 +30,42 @@ const mockExhibitionsData = [
   }
 ];
 
-const ExhibitionItem = ({ text, imageUrl, id, isFeatured }) =>
-  <div className={styles.item}>
-    <img alt={text} className={styles.itemImg} src={imageUrl} />
-    <p className={styles.itemText}>{text}</p>
+const NextArrow = ({ style, onClick }) =>
+  <div
+    className={styles.nextArrow}
+    style={{ ...style, background: "red" }}
+    onClick={onClick}
+  >
+    Next
   </div>;
 
-// decorators={Carousel.getDefaultProps().decorators.slice(0, 2)}
+const PrevArrow = ({ style, onClick }) =>
+  <div
+    className={styles.prevArrow}
+    style={{ ...style, background: "red" }}
+    onClick={onClick}
+  >
+    Previous
+  </div>;
+
 const OnlineExhibitions = (/* data */) =>
   <div className={styles.wrapper}>
     <div className={styles.onlineExhibitions}>
       <h1 className={styles.header}>Online Exhibitions</h1>
-      <div className={styles.items}>
-        <Carousel slidesToShow={3} slidesToScroll="auto" cellSpacing={12}>
-          {mockExhibitionsData.map(({ text, imageUrl, id, isFeatured }) =>
-            <ExhibitionItem
-              key={id}
-              text={text}
-              imageUrl={imageUrl}
-              id={id}
-              isFeatured={isFeatured}
-            />
-          )}
-        </Carousel>
-      </div>
+      <Slider
+        slidesToShow={3}
+        nextArrow={<NextArrow />}
+        prevArrow={<PrevArrow />}
+        draggable={false}
+        slidesToScroll={1}
+      >
+        {mockExhibitionsData.map(({ text, imageUrl, id, isFeatured }) =>
+          <div className={styles.item}>
+            <img alt={text} className={styles.itemImg} src={imageUrl} />
+            <p className={styles.itemText}>{text}</p>
+          </div>
+        )}
+      </Slider>
     </div>
   </div>;
 
