ff2e99f563a575b1c25c8a7612d60e63a291bb5d
Frankie Simms <fdsimms@gmail.com>
2017-06-30 15:16:03 -0400

    wip

--- src/components/Home/modules/OnlineExhibitions/index.js
+++ src/components/Home/modules/OnlineExhibitions/index.js
@@ -1,10 +1,11 @@
 import React from "react";
+import Carousel from "nuka-carousel";
 
 import styles from "./OnlineExhibitions.css";
 
 const mockExhibitionsData = [
   {
-    text: "Building the First Transcontinental Railroad",
+    text: "In Focus: The Evolution of the Personal Camera",
     imageUrl: "http://via.placeholder.com/334x243",
     id: 0,
     isFeatured: true
@@ -20,30 +21,39 @@ const mockExhibitionsData = [
     imageUrl: "http://via.placeholder.com/334x243",
     id: 2,
     isFeatured: false
+  },
+  {
+    text: "Design Tenets",
+    imageUrl: "http://via.placeholder.com/334x243",
+    id: 2,
+    isFeatured: false
   }
 ];
 
 const ExhibitionItem = ({ text, imageUrl, id, isFeatured }) =>
-  <li className={styles.item}>
+  <div className={styles.item}>
     <img alt={text} className={styles.itemImg} src={imageUrl} />
     <p className={styles.itemText}>{text}</p>
-  </li>;
+  </div>;
 
+// decorators={Carousel.getDefaultProps().decorators.slice(0, 2)}
 const OnlineExhibitions = (/* data */) =>
   <div className={styles.wrapper}>
     <div className={styles.onlineExhibitions}>
       <h1 className={styles.header}>Online Exhibitions</h1>
-      <ul className={styles.items}>
-        {mockExhibitionsData.map(({ text, imageUrl, id, isFeatured }) =>
-          <ExhibitionItem
-            key={id}
-            text={text}
-            imageUrl={imageUrl}
-            id={id}
-            isFeatured={isFeatured}
-          />
-        )}
-      </ul>
+      <div className={styles.items}>
+        <Carousel slidesToShow={3} slidesToScroll="auto" cellSpacing={12}>
+          {mockExhibitionsData.map(({ text, imageUrl, id, isFeatured }) =>
+            <ExhibitionItem
+              key={id}
+              text={text}
+              imageUrl={imageUrl}
+              id={id}
+              isFeatured={isFeatured}
+            />
+          )}
+        </Carousel>
+      </div>
     </div>
   </div>;
 
