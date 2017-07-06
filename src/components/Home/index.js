10a26bb3698d0dca8255c790407000687d975fef
Frankie Simms <fdsimms@gmail.com>
2017-07-06 10:19:45 -0400

    wip

--- src/components/Home/index.js
+++ src/components/Home/index.js
@@ -4,12 +4,14 @@ import styles from "./Home.css";
 import LandingSection from "./modules/LandingSection";
 import OnlineExhibitions from "./modules/OnlineExhibitions";
 import PrimarySourceSets from "./modules/PrimarySourceSets";
+import DPLAUsers from "./modules/DPLAUsers";
 
 const Home = () =>
   <div className={styles.home}>
     <LandingSection />
     <OnlineExhibitions />
     <PrimarySourceSets />
+    <DPLAUsers />
   </div>;
 
 export default Home;
---
+++ src/components/Home/modules/DPLAUsers/index.js
@@ -0,0 +1,100 @@
+import React from "react";
+import Slider from "react-slick";
+
+import styles from "./DPLAUsers.css";
+
+const NextArrow = ({ onClick }) =>
+  <div className={styles.nextArrow} onClick={onClick}>
+    ⇨
+  </div>;
+
+const PrevArrow = ({ onClick }) =>
+  <div className={styles.prevArrow} onClick={onClick}>
+    ⇦
+  </div>;
+
+const DPLAUsers = (/* data */) =>
+  <div className={styles.wrapper}>
+    <div className={styles.DPLAUsers}>
+      <h1 className={styles.header}>Who Uses DPLA?</h1>
+      <div className={styles.divider} />
+      <Slider
+        slidesToShow={3}
+        infinite={false}
+        adaptiveHeight={true}
+        nextArrow={<NextArrow />}
+        prevArrow={<PrevArrow />}
+        draggable={false}
+        slidesToScroll={1}
+      >
+        <div className={[styles.item, styles.learners].join(" ")}>
+          <div className={styles.imageDiv}>
+            <img
+              alt="text"
+              className={styles.itemImg}
+              src="http://via.placeholder.com/187x214"
+            />
+          </div>
+          <div className={styles.content}>
+            <h1 className={styles.itemHeader}>Lifelong learners</h1>
+            <p className={styles.text}>
+              Lifelong learners will enjoy browsing Exhibitions or using our
+              Topic Browse index to find library artifacts related to their
+              interests.
+            </p>
+          </div>
+        </div>
+        <div className={[styles.item, styles.educators].join(" ")}>
+          <div className={styles.imageDiv}>
+            <img
+              alt="text"
+              className={styles.itemImg}
+              src="http://via.placeholder.com/187x214"
+            />
+          </div>
+          <div className={styles.content}>
+            <h1 className={styles.itemHeader}>Educators</h1>
+            <p className={styles.text}>
+              The DPLA content team creates curated Primary Source Sets,
+              complete with teacher’s notes for use in the classroom.
+            </p>
+          </div>
+        </div>
+        <div className={[styles.item, styles.genealogists].join(" ")}>
+          <div className={styles.imageDiv}>
+            <img
+              alt="text"
+              className={styles.itemImg}
+              src="http://via.placeholder.com/187x214/ffffff"
+            />
+          </div>
+          <div className={styles.content}>
+            <h1 className={styles.itemHeader}>Genealogists</h1>
+            <p className={styles.text}>
+              Genealogists and historians enjoy using our search tools and
+              filter controls for finding artifacts related to their family or
+              research topic.
+            </p>
+          </div>
+        </div>
+        <div className={[styles.item, styles.researchers].join(" ")}>
+          <div className={styles.imageDiv}>
+            <img
+              alt="text"
+              className={styles.itemImg}
+              src="http://via.placeholder.com/187x214/ffffff"
+            />
+          </div>
+          <div className={styles.content}>
+            <h1 className={styles.itemHeader}>Researchers</h1>
+            <p className={styles.text}>
+              Researchers Lorem Ipsum is simply dummy text of the printing and
+              typesetting industry. Lorem Ipsum has been the industry's stand
+            </p>
+          </div>
+        </div>
+      </Slider>
+    </div>
+  </div>;
+
+export default DPLAUsers;
