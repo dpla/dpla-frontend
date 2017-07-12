0f5a654ec9cd6de1a66d8d768844e8df44ed0117
Frankie Simms <fdsimms@gmail.com>
2017-07-12 16:05:55 -0400

    feat: header search bar

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React from "react";
 import { BrowserRouter as Router, Route } from "react-router-dom";
 import Header from "./components/Header";
+import HeaderSearchBar from "./components/HeaderSearchBar";
 import Footer from "./components/Footer";
 import Home from "./components/Home";
 
@@ -10,7 +11,9 @@ const App = () =>
       <Header />
 
       <Route path="/" exact={true} component={Home} />
-
+      <Route path="/(.+)">
+        <HeaderSearchBar />
+      </Route>
       <Footer />
     </div>
   </Router>;
