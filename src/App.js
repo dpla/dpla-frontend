3c8b12d9c12957d7bb0c6db83df35f5fa17e69d2
Frankie Simms <fdsimms@gmail.com>
2017-07-14 13:17:06 -0400

    feat: primary source sets landing page
    * feat: header search bar
    * feat: routing
    * feat: primary source sets landing page

--- src/App.js
+++ src/App.js
@@ -1,8 +1,10 @@
 import React from "react";
 import { BrowserRouter as Router, Route } from "react-router-dom";
 import Header from "./components/Header";
+import HeaderSearchBar from "./components/HeaderSearchBar";
 import Footer from "./components/Footer";
 import Home from "./components/Home";
+import PrimarySourceSets from "./components/PrimarySourceSets";
 
 const App = () =>
   <Router>
@@ -10,6 +12,15 @@ const App = () =>
       <Header />
 
       <Route path="/" exact={true} component={Home} />
+
+      <Route
+        path="/(.+)"
+        render={props =>
+          <div>
+            <HeaderSearchBar />
+            <Route path="/primary-source-sets" component={PrimarySourceSets} />
+          </div>}
+      />
 
       <Footer />
     </div>
---
+++ src/components/PrimarySourceSets/index.js
@@ -0,0 +1,20 @@
+import React from "react";
+
+import styles from "./PrimarySourceSets.css";
+
+import Filters from "./modules/Filters";
+import Header from "./modules/Header";
+import Sets from "./modules/Sets";
+import OtherInfo from "./modules/OtherInfo";
+
+import mockSets from "./mockSets";
+
+const PrimarySourceSets = () =>
+  <div className={styles.primarySourceSets}>
+    <Header />
+    <Filters />
+    <Sets sets={mockSets} />
+    <OtherInfo />
+  </div>;
+
+export default PrimarySourceSets;
