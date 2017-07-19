ebcaba3639edf8db210104c0441a512fc5fd3688
Frankie Simms <fdsimms@gmail.com>
2017-07-19 11:19:42 -0400

    wip

--- src/components/PrimarySourceSet/modules/ResourcesTabs/index.js
+++ src/components/PrimarySourceSet/modules/ResourcesTabs/index.js
@@ -7,39 +7,69 @@ import { module } from "../../../../utils.css";
 
 import SourceSetContent from "../SourceSetContent";
 
-const ResourcesTabs = ({ routerProps }) =>
-  <div className={styles.wrapper}>
-    <div className={styles.tabsWrapper}>
-      <div className={[styles.tabs, module].join(" ")}>
-        <Link to={`${routerProps.match.url}/source-set`} className={styles.tab}>
-          Source Set
-        </Link>
-        <Link
-          to={`${routerProps.match.url}/additional-resources`}
-          className={styles.tab}
-        >
-          Additional Resources
-        </Link>
-        <Link
-          to={`${routerProps.match.url}/teaching-guide`}
-          className={styles.tab}
-        >
-          Teaching Guide
-        </Link>
+class ResourcesTabs extends React.Component {
+  componentWillMount() {
+    this.setState({ currentTab: "sourceSet" });
+  }
+
+  onTabChange(newTab) {
+    this.setState({ currentTab: newTab });
+  }
+
+  render() {
+    const { routerProps } = this.props;
+    return (
+      <div className={styles.wrapper}>
+        <div className={styles.tabsWrapper}>
+          <div className={[styles.tabs, module].join(" ")}>
+            <Link
+              to={`${routerProps.match.url}/source-set`}
+              onClick={() => this.onTabChange("sourceSet")}
+              className={[
+                styles.tab,
+                this.state.currentTab === "sourceSet" && styles.activeTab
+              ].join(" ")}
+            >
+              Source Set
+            </Link>
+            <Link
+              to={`${routerProps.match.url}/additional-resources`}
+              onClick={() => this.onTabChange("additionalResources")}
+              className={[
+                styles.tab,
+                this.state.currentTab === "additionalResources" &&
+                  styles.activeTab
+              ].join(" ")}
+            >
+              Additional Resources
+            </Link>
+            <Link
+              to={`${routerProps.match.url}/teaching-guide`}
+              onClick={() => this.onTabChange("teachingGuide")}
+              className={[
+                styles.tab,
+                this.state.currentTab === "teachingGuide" && styles.activeTab
+              ].join(" ")}
+            >
+              Teaching Guide
+            </Link>
+          </div>
+        </div>
+        <div className={[styles.resourcesTabs, module].join(" ")}>
+          <Route
+            path="/primary-source-sets/:id/"
+            exact={true}
+            component={SourceSetContent}
+          />
+          <Route
+            path="/primary-source-sets/:id/source-set"
+            exact={true}
+            component={SourceSetContent}
+          />
+        </div>
       </div>
-    </div>
-    <div className={[styles.resourcesTabs, module].join(" ")}>
-      <Route
-        path="/primary-source-sets/:id/"
-        exact={true}
-        component={SourceSetContent}
-      />
-      <Route
-        path="/primary-source-sets/:id/source-set"
-        exact={true}
-        component={SourceSetContent}
-      />
-    </div>
-  </div>;
+    );
+  }
+}
 
 export default ResourcesTabs;
