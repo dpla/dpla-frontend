4c48b59c0b2c7e62224438543609ff2a9fa6aede
Frankie Simms <fdsimms@gmail.com>
2017-07-26 15:47:57 -0400

    wip

--- src/screens/App/screens/Main/screens/TopicBrowse/screens/TopicListing/index.js
+++ src/screens/App/screens/Main/screens/TopicBrowse/screens/TopicListing/index.js
@@ -2,15 +2,17 @@ import React from "react";
 
 import styles from "./TopicListing.css";
 
-// import Filters from "./components/Filters";
+import Filters from "./components/Filters";
 import Header from "./components/Header";
-// import Topics from "./components/Topics";
-//
-// import mockTopics from "./mockTopics";
+import Topics from "./components/Topics";
+
+import mockTopics from "./mockTopics";
 
 const AllTopics = () =>
   <div className={styles.allTopics}>
     <Header />
+    <Filters />
+    <Topics topics={mockTopics} />
   </div>;
 
 export default AllTopics;
