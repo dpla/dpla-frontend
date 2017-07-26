b4560ef33fbd0b60a2405ee5a8763658be972a3b
Frankie Simms <fdsimms@gmail.com>
2017-07-26 10:44:30 -0400

    wip

--- src/reducers/primarySourceSets.js
+++ src/reducers/primarySourceSets.js
@@ -1,7 +1,5 @@
-const primarySourceSets = (state = { primarySourceSets: [] }, action) => {
+const primarySourceSets = (state = { primarySourceSets: {} }, action) => {
   switch (action.type) {
-    case "UPDATE_PRIMARY_SOURCE_SETS":
-      return Object.assign(state, { primarySourceSets: action.sets });
     default:
       return state;
   }
