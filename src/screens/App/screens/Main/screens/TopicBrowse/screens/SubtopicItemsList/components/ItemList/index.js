1321d68de78c74c66bc4eb557d42a64ff2f6f635
Frankie Simms <fdsimms@gmail.com>
2017-07-31 14:25:52 -0400

    wip

--- src/screens/App/screens/Main/screens/TopicBrowse/screens/SubtopicItemsList/components/ItemList/index.js
+++ src/screens/App/screens/Main/screens/TopicBrowse/screens/SubtopicItemsList/components/ItemList/index.js
@@ -2,19 +2,60 @@ import React from "react";
 
 import styles from "./ItemList.css";
 
-const ItemList = ({ items }) =>
-  <div className={styles.itemList}>
-    <div className={styles.itemListHeader}>
-      <p className={styles.itemCount}>{items.length} Items</p>
-      <div className={styles.viewButtons}>
-        <button classname={styles.viewButton}>
-          <img className={styles.viewButtonIcon} src="" alt="Grid View" />
-        </button>
-        <button classname={styles.viewButton}>
-          <img className={styles.viewButtonIcon} src="" alt="Grid View" />
-        </button>
+import gridViewIcon from "../../../../../../../../../../images/grid-view-icon.svg";
+import listViewIcon from "../../../../../../../../../../images/list-view-icon.svg";
+
+class ItemList extends React.Component {
+  componentWillMount() {
+    this.setState({ view: "grid" });
+  }
+
+  updateView = view => this.setState({ view });
+
+  render() {
+    const { items } = this.props;
+    const { view } = this.state;
+
+    return (
+      <div className={styles.itemList}>
+        <div className={styles.itemListHeader}>
+          <p className={styles.itemCount}>{items.length} Items</p>
+          <div className={styles.viewButtons}>
+            <button
+              className={[
+                styles.listViewButton,
+                view === "list"
+                  ? styles.viewButtonActive
+                  : styles.viewButtonInactive
+              ].join(" ")}
+              onClick={() => this.updateView("list")}
+            >
+              <img
+                className={styles.viewButtonIcon}
+                src={listViewIcon}
+                alt="List View"
+              />
+            </button>
+            <button
+              className={[
+                styles.gridViewButton,
+                view === "grid"
+                  ? styles.viewButtonActive
+                  : styles.viewButtonInactive
+              ].join(" ")}
+              onClick={() => this.updateView("grid")}
+            >
+              <img
+                className={styles.viewButtonIcon}
+                src={gridViewIcon}
+                alt="Grid View"
+              />
+            </button>
+          </div>
+        </div>
       </div>
-    </div>
-  </div>;
+    );
+  }
+}
 
 export default ItemList;
