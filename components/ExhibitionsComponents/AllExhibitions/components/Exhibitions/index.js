87072690d5748ec5206f21ca4b97ca4785f95cc1
Frankie Simms <fdsimms@gmail.com>
2017-08-09 17:51:27 -0400

    wip

--- components/ExhibitionsComponents/AllExhibitions/components/Exhibitions/index.js
+++ components/ExhibitionsComponents/AllExhibitions/components/Exhibitions/index.js
@@ -1,14 +1,26 @@
 import React from "react";
-// import Link from "next/link";
+// import Link from "next/divnk";
 
 import { classNames, stylesheet } from "./Exhibitions.css";
 import { classNames as utilClassNames } from "css/utils.css";
 
 const { module } = utilClassNames;
 
+const Exhibition = ({ className, exhibition }) =>
+  <div className={[classNames.exhibition, classNames[className]].join(" ")}>
+    <img
+      className={classNames.image}
+      alt={exhibition.title}
+      src={exhibition.image}
+    />
+    <div className={classNames.overlay}>
+      <p className={classNames.exhibitionTitle}>{exhibition.title}</p>
+    </div>
+  </div>;
+
 const Exhibitions = ({ exhibitions, route }) =>
   <div className={[module, classNames.exhibitionsWrapper].join(" ")}>
-    <style dangerouslySetInnerHTML={{ __html: stylesheet }} />
+    hey
   </div>;
 
 export default Exhibitions;
