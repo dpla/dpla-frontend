a6a9cd7299efdedfb71311f4afd6a8337896a8ef
Frankie Simms <fdsimms@gmail.com>
2017-08-24 11:28:53 -0400

    wip

--- components/shared/Accordion/index.js
+++ components/shared/Accordion/index.js
@@ -2,6 +2,9 @@ import React from "react";
 
 import { stylesheet, classNames } from "./Accordion.css";
 
+const addIcon = "/static/images/add.svg";
+const subtractIcon = "/static/images/subtract.svg";
+
 class Accordion extends React.Component {
   componentWillMount() {
     this.setState({ items: this.props.items });
@@ -24,27 +27,28 @@ class Accordion extends React.Component {
       <div>
         {this.state.items.map((item, i) =>
           <div
-            className={
-              item.active
-                ? [classNames.active, this.props.activeItemClass].join(" ")
-                : [classNames.inactive, this.props.inactiveItemClass].join(" ")
-            }
+            className={item.active ? classNames.active : classNames.inactive}
           >
             <button
-              className={this.props.itemHeaderClass}
+              className={classNames.itemHeader}
               onClick={() => this.onClickItem(i)}
             >
-              {item.name}
+              <span>{item.name}</span>
+              {item.active &&
+                <img
+                  src={subtractIcon}
+                  alt={item.name}
+                  className={classNames.subtractIcon}
+                />}
+              {!item.active &&
+                <img
+                  src={addIcon}
+                  alt={item.name}
+                  className={classNames.addIcon}
+                />}
             </button>
-            <ul
-              className={[
-                this.props.subitemsWrapperClass,
-                classNames.subitems
-              ].join(" ")}
-            >
-              {item.subitems.map(subitem =>
-                <li className={this.props.subitemClass}>{subitem.content}</li>
-              )}
+            <ul className={classNames.subitems}>
+              {item.subitems.map(subitem => <li>{subitem.content}</li>)}
             </ul>
           </div>
         )}
