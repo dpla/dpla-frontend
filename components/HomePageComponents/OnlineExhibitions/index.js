a7871626b8d03860caa109b7cba07c19e2b784e8
Neil Renicker <neilrenicker@gmail.com>
2017-08-29 12:55:48 -0400

    wip: online exhibitions slider responsive

--- components/HomePageComponents/OnlineExhibitions/index.js
+++ components/HomePageComponents/OnlineExhibitions/index.js
@@ -3,66 +3,68 @@ import Slider from "react-slick";
 import Link from "next/link";
 
 import { stylesheet, classNames } from "./OnlineExhibitions.css";
+import { classNames as breakpoints } from "css/breakpoints.css";
 
-import breakpoints from "css/breakpoints.css";
 const smallChevron = "static/images/chevron-thick-orange.svg";
 const largeChevron = "static/images/chevron-thin.svg";
+const sampleImage = "static/images/sample-image.jpeg";
 
 const mockExhibitionsData = [
   {
     text: "Building the First Transcontinental Railroad",
-    imageUrl: "http://lorempixel.com/334/241/food",
+    imageUrl: sampleImage,
     id: 0,
     isFeatured: true
   },
   {
     text: "Race to the Moon",
-    imageUrl: "http://lorempixel.com/334/242/food",
+    imageUrl: sampleImage,
     id: 1,
     isFeatured: false
   },
   {
     text: "In Focus: The Evolution of the Personal Camera",
-    imageUrl: "http://lorempixel.com/334/243/food",
+    imageUrl: sampleImage,
     id: 2,
     isFeatured: false
   },
   {
     text: "Design Tenets",
-    imageUrl: "http://lorempixel.com/334/244/food",
+    imageUrl: sampleImage,
     id: 2,
     isFeatured: false
   }
 ];
 
 const NextArrow = ({ onClick, className }) =>
-  <button className={classNames.nextArrow} onClick={onClick}>
-    <img
-      alt=""
-      className={[classNames.nextChevron, className].join(" ")}
-      src={largeChevron}
-    />
+  <button
+    className={`${classNames.arrow} ${classNames.nextArrow} ${className}`}
+    onClick={onClick}
+  >
+    <img alt="Right-pointing arrow" src={largeChevron} />
   </button>;
 
 const PrevArrow = ({ onClick, className }) =>
-  <button className={classNames.prevArrow} onClick={onClick}>
-    <img
-      alt=""
-      className={[classNames.backChevron, className].join(" ")}
-      src={largeChevron}
-    />
+  <button
+    className={`${classNames.arrow} ${classNames.prevArrow} ${className}`}
+    onClick={onClick}
+  >
+    <img alt="Left-pointing arrow" src={largeChevron} />
   </button>;
 
 const OnlineExhibitions = (/* data */) =>
   <div className={classNames.wrapper}>
-    <div className={classNames.onlineExhibitions}>
-      <div className={classNames.headerAndBrowse}>
-        <h1 className={classNames.header}>Online Exhibitions</h1>
-        <Link to="">
-          <a className={classNames.browse}>
-            <span>Browse all Exhibitions</span>
+    <div className={`${classNames.content} site-max-width`}>
+      <div className={classNames.heading}>
+        <h1 className={classNames.title}>Online Exhibitions</h1>
+        <Link href="/exhibitions">
+          <a className={classNames.moreLink}>
+            <span>
+              Browse all{" "}
+              <span className={classNames.moreLinkNoun}>Exhibitions</span>
+            </span>
             <img
-              alt="browse exhibitions"
+              alt="Browse exhibitions"
               className={classNames.smallChevron}
               src={smallChevron}
             />
@@ -78,11 +80,9 @@ const OnlineExhibitions = (/* data */) =>
         slidesToScroll={1}
         responsive={[
           {
-            breakpoint: parseInt(breakpoints.smallPx, 10),
+            breakpoint: ~~breakpoints.smallPx,
             settings: {
-              centerMode: true,
-              centerPadding: "15.5%",
-              slidesToShow: 1.33,
+              slidesToShow: 1.125,
               arrows: false,
               draggable: true
             }
@@ -92,7 +92,8 @@ const OnlineExhibitions = (/* data */) =>
         {mockExhibitionsData.map(({ text, imageUrl, id, isFeatured }) =>
           <div className={classNames.item}>
             <img alt={text} className={classNames.itemImg} src={imageUrl} />
-            {isFeatured && <div className={classNames.featured}>Featured</div>}
+            {isFeatured &&
+              <div className={classNames.featuredTag}>Featured</div>}
             <p className={classNames.itemText}>
               {text}
             </p>
