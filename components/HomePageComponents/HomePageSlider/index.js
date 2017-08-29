ceb57164eac0db396ef39e62529cb6078c8a997c
Neil Renicker <neilrenicker@gmail.com>
2017-08-29 13:42:47 -0400

    wip: share slider components on home page

--- components/HomePageComponents/HomePageSlider/index.js
+++ components/HomePageComponents/HomePageSlider/index.js
@@ -1,40 +1,14 @@
 import React from "react";
 import Slider from "react-slick";
 import Link from "next/link";
+import { markdown } from "markdown";
 
-import { stylesheet, classNames } from "./OnlineExhibitions.css";
+import { stylesheet, classNames } from "./HomePageSlider.css";
 import { classNames as breakpoints } from "css/breakpoints.css";
 
-const smallChevron = "static/images/chevron-thick-orange.svg";
+const moreLinkChevron = "static/images/chevron-thick-orange.svg";
+const moreLinkChevronBlue = "static/images/chevron-thick-blue.svg";
 const largeChevron = "static/images/chevron-thin.svg";
-const sampleImage = "static/images/sample-image.jpeg";
-
-const mockExhibitionsData = [
-  {
-    text: "Building the First Transcontinental Railroad",
-    imageUrl: sampleImage,
-    id: 0,
-    isFeatured: true
-  },
-  {
-    text: "Race to the Moon",
-    imageUrl: sampleImage,
-    id: 1,
-    isFeatured: false
-  },
-  {
-    text: "In Focus: The Evolution of the Personal Camera",
-    imageUrl: sampleImage,
-    id: 2,
-    isFeatured: false
-  },
-  {
-    text: "Design Tenets",
-    imageUrl: sampleImage,
-    id: 2,
-    isFeatured: false
-  }
-];
 
 const NextArrow = ({ onClick, className }) =>
   <button
@@ -52,27 +26,38 @@ const PrevArrow = ({ onClick, className }) =>
     <img alt="Left-pointing arrow" src={largeChevron} />
   </button>;
 
-const OnlineExhibitions = (/* data */) =>
-  <div className={classNames.wrapper}>
+const HomePageSlider = ({
+  browseLinkName,
+  browseLinkUrl,
+  items,
+  slidesToShow,
+  title,
+  theme
+}) =>
+  <div
+    className={`${classNames.wrapper} ${theme === "blue"
+      ? classNames.theme_blue
+      : ""} `}
+  >
     <div className={`${classNames.content} site-max-width`}>
       <div className={classNames.heading}>
-        <h1 className={classNames.title}>Online Exhibitions</h1>
-        <Link href="/exhibitions">
+        <h1 className={classNames.title}>{title}</h1>
+        <Link href={browseLinkUrl}>
           <a className={classNames.moreLink}>
             <span>
               Browse all{" "}
-              <span className={classNames.moreLinkNoun}>Exhibitions</span>
+              <span className={classNames.moreLinkNoun}>{browseLinkName}</span>
             </span>
             <img
               alt="Browse exhibitions"
-              className={classNames.smallChevron}
-              src={smallChevron}
+              className={classNames.moreLinkChevron}
+              src={theme === "blue" ? moreLinkChevronBlue : moreLinkChevron}
             />
           </a>
         </Link>
       </div>
       <Slider
-        slidesToShow={3}
+        slidesToShow={slidesToShow ? slidesToShow : 3}
         infinite={false}
         nextArrow={<NextArrow />}
         prevArrow={<PrevArrow />}
@@ -89,14 +74,15 @@ const OnlineExhibitions = (/* data */) =>
           }
         ]}
       >
-        {mockExhibitionsData.map(({ text, imageUrl, id, isFeatured }) =>
-          <div className={classNames.item}>
-            <img alt={text} className={classNames.itemImg} src={imageUrl} />
+        {items.map(({ name, thumbnailUrl, isFeatured }, index) =>
+          <div className={classNames.item} key={index}>
+            <img alt={name} className={classNames.itemImg} src={thumbnailUrl} />
             {isFeatured &&
               <div className={classNames.featuredTag}>Featured</div>}
-            <p className={classNames.itemText}>
-              {text}
-            </p>
+            <div
+              dangerouslySetInnerHTML={{ __html: markdown.toHTML(name) }}
+              className={classNames.itemText}
+            />
           </div>
         )}
       </Slider>
@@ -104,4 +90,4 @@ const OnlineExhibitions = (/* data */) =>
     <style dangerouslySetInnerHTML={{ __html: stylesheet }} />
   </div>;
 
-export default OnlineExhibitions;
+export default HomePageSlider;
--- pages/index.js
+++ pages/index.js
@@ -3,17 +3,58 @@ import fetch from "isomorphic-fetch";
 
 import MainLayout from "../components/MainLayout";
 import HomeHero from "../components/HomePageComponents/HomeHero";
-import OnlineExhibitions from "../components/HomePageComponents/OnlineExhibitions";
-import PrimarySourceSets from "../components/HomePageComponents/PrimarySourceSets";
+import HomePageSlider from "../components/HomePageComponents/HomePageSlider";
 import DPLAUsers from "../components/HomePageComponents/DPLAUsers";
 import SocialMedia from "../components/HomePageComponents/SocialMedia";
 import FromTheBlog from "../components/HomePageComponents/FromTheBlog";
 
+// TODO: remove when we have real data for exhibitions
+const sampleImage = "static/images/sample-image.jpeg";
+
+const mockExhibitionsData = [
+  {
+    name: "Building the First Transcontinental Railroad",
+    thumbnailUrl: sampleImage,
+    id: 0,
+    isFeatured: true
+  },
+  {
+    name: "Race to the Moon",
+    thumbnailUrl: sampleImage,
+    id: 1,
+    isFeatured: false
+  },
+  {
+    name: "In Focus: The Evolution of the Personal Camera",
+    thumbnailUrl: sampleImage,
+    id: 2,
+    isFeatured: false
+  },
+  {
+    name: "Design Tenets",
+    thumbnailUrl: sampleImage,
+    id: 3,
+    isFeatured: false
+  }
+];
+
 const Home = ({ sourceSets }) =>
   <MainLayout hideSearchBar>
     <HomeHero />
-    <OnlineExhibitions />
-    <PrimarySourceSets sourceSets={sourceSets} />
+    <HomePageSlider
+      items={mockExhibitionsData}
+      title="Online Exhibitions"
+      browseLinkUrl="/exhibitions"
+      browseLinkName="Exhibitions"
+    />
+    <HomePageSlider
+      items={sourceSets}
+      title="Primary Source Sets"
+      browseLinkUrl="/primary-source-sets"
+      browseLinkName="Sets"
+      slidesToShow={4}
+      theme="blue"
+    />
     <DPLAUsers />
     <SocialMedia />
     <FromTheBlog />
