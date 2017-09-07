e1f99e6a957b142fa3d2a592887040f2e122a1d4
Frankie Simms <fdsimms@gmail.com>
2017-09-07 13:15:29 -0400

    feat: clearable search filters list

---
+++ constants/search.js
@@ -0,0 +1,8 @@
+export const possibleFacets = [
+  "sourceResource.type",
+  "sourceResource.subject.name",
+  "sourceResource.spatial.name",
+  "sourceResource.language.name",
+  "dataProvider",
+  "provider.name"
+];
