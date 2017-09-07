dbdb73df2ae9e1f44ffc22d517b98249481cee47
Frankie Simms <fdsimms@gmail.com>
2017-09-06 21:30:36 -0400

    wip

--- pages/search/index.js
+++ pages/search/index.js
@@ -9,38 +9,32 @@ import {
   stylesheet
 } from "components/SearchComponents/SearchComponents.css";
 
-class Search extends React.Component {
-  render() {
-    const { url, results } = this.props;
+const Search = ({ url, results }) =>
+  <MainLayout route={url}>
+    <div className={classNames.wrapper}>
+      <OptionsBar route={url} itemCount={results.count} />
+      <MainContent
+        paginationInfo={{
+          pageCount: results.count,
+          pageSize: url.query.page_size || 10,
+          currentPage: url.query.page || 1
+        }}
+        route={url}
+        facets={results.facets}
+        results={results.docs.map(doc =>
+          Object.assign({}, doc.sourceResource, {
+            thumbnailUrl: Array.isArray(doc.object)
+              ? doc.object[0]
+              : doc.object,
+            sourceUrl: doc.isShownAt
+          })
+        )}
+      />
+    </div>
+    <style dangerouslySetInnerHTML={{ __html: stylesheet }} />
+  </MainLayout>;
 
-    return (
-      <MainLayout route={url}>
-        <div className={classNames.wrapper}>
-          <OptionsBar route={url} itemCount={results.count} />
-          <MainContent
-            paginationInfo={{
-              pageCount: results.count,
-              pageSize: url.query.page_size || 10,
-              currentPage: url.query.page || 1
-            }}
-            route={url}
-            facets={results.facets}
-            results={results.docs.map(doc =>
-              Object.assign({}, doc.sourceResource, {
-                thumbnailUrl: Array.isArray(doc.object)
-                  ? doc.object[0]
-                  : doc.object,
-                sourceUrl: doc.isShownAt
-              })
-            )}
-          />
-        </div>
-        <style dangerouslySetInnerHTML={{ __html: stylesheet }} />
-      </MainLayout>
-    );
-  }
-}
-
+// TODO: better API key storage?
 const API_KEY = "fb4132db4a42b89f14effa41bf280672";
 Search.getInitialProps = async ({ query }) => {
   // TODO: clean this up
