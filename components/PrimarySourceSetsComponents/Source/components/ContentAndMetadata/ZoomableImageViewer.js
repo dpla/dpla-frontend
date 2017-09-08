c3948bcdb2e2a7f99f0eb8ea14ae6894afc0d750
Frankie Simms <fdsimms@gmail.com>
2017-09-08 11:23:32 -0400

    wip

--- components/PrimarySourceSetsComponents/Source/components/ContentAndMetadata/ZoomableImageViewer.js
+++ components/PrimarySourceSetsComponents/Source/components/ContentAndMetadata/ZoomableImageViewer.js
@@ -4,39 +4,29 @@ import ReactDOM from "react-dom/server";
 import { classNames } from "./ContentAndMetadata.css";
 
 // TODO: maybe move this into a shared directory
-// https://github.com/facebook/react/issues/1252
+//  idea from https://github.com/facebook/react/issues/1252
 const Noscript = ({ children }) => {
   const staticMarkup = ReactDOM.renderToStaticMarkup(children);
   return <noscript dangerouslySetInnerHTML={{ __html: staticMarkup }} />;
 };
 
+const initOpenSeaDragon = url =>
+  this.OpenSeaDragon({
+    id: "openseadragon1",
+    tileSources: { type: "image", url },
+    prefixUrl: "/static/images/openseadragon/"
+  });
+
 export default class ZoomableImageViewer extends React.Component {
   componentDidMount() {
-    // const OpenSeaDragon = require("static/openseadragon/openseadragon.min.js");
     this.OpenSeaDragon = require("openseadragon");
-    this.viewer = this.OpenSeaDragon({
-      //eslint-disable-line
-      id: "openseadragon1",
-      tileSources: {
-        type: "image",
-        url: this.props.pathToFile
-      },
-      prefixUrl: "/static/images/openseadragon/"
-    });
+    this.viewer = initOpenSeaDragon(this.props.pathToFile);
   }
 
   componentWillReceiveProps(nextProps) {
     if (nextProps.pathToFile !== this.props.pathToFile) {
       this.viewer.destroy();
-      this.viewer = this.OpenSeaDragon({
-        //eslint-disable-line
-        id: "openseadragon1",
-        tileSources: {
-          type: "image",
-          url: nextProps.pathToFile
-        },
-        prefixUrl: "/static/images/openseadragon/"
-      });
+      this.viewer = initOpenSeaDragon(nextProps.pathToFile);
     }
   }
 
