44c81722d12acafa61eaee4822f4df9220f6edf5
Frankie Simms <fdsimms@gmail.com>
2017-09-08 14:50:57 -0400

    fix: redraw openseadragon viewer on image change

--- components/PrimarySourceSetsComponents/Source/components/ContentAndMetadata/ZoomableImageViewer.js
+++ components/PrimarySourceSetsComponents/Source/components/ContentAndMetadata/ZoomableImageViewer.js
@@ -4,25 +4,31 @@ import ReactDOM from "react-dom/server";
 import { classNames } from "./ContentAndMetadata.css";
 
 // TODO: maybe move this into a shared directory
-// https://github.com/facebook/react/issues/1252
+//  idea from https://github.com/facebook/react/issues/1252
 const Noscript = ({ children }) => {
   const staticMarkup = ReactDOM.renderToStaticMarkup(children);
   return <noscript dangerouslySetInnerHTML={{ __html: staticMarkup }} />;
 };
 
+const initOpenSeaDragon = url => {
+  const OpenSeaDragon = require("openseadragon");
+  return OpenSeaDragon({
+    id: "openseadragon1",
+    tileSources: { type: "image", url },
+    prefixUrl: "/static/images/openseadragon/"
+  });
+};
+
 export default class ZoomableImageViewer extends React.Component {
   componentDidMount() {
-    // const OpenSeaDragon = require("static/openseadragon/openseadragon.min.js");
-    const OpenSeaDragon = require("openseadragon");
-    this.viewer = OpenSeaDragon({
-      //eslint-disable-line
-      id: "openseadragon1",
-      tileSources: {
-        type: "image",
-        url: this.props.pathToFile
-      },
-      prefixUrl: "/static/images/openseadragon/"
-    });
+    this.viewer = initOpenSeaDragon(this.props.pathToFile);
+  }
+
+  componentWillReceiveProps(nextProps) {
+    if (nextProps.pathToFile !== this.props.pathToFile) {
+      this.viewer.destroy();
+      this.viewer = initOpenSeaDragon(nextProps.pathToFile);
+    }
   }
 
   render() {
