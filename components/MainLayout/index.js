63e18940a91049d7910a95ceec7935a735569b60
Neil Renicker <neilrenicker@gmail.com>
2017-09-12 10:05:45 -0400

    feat: small screen navigation (#105)
    * feat: mobile nav
    
    * wip: allow scrolls when screen widens

--- components/MainLayout/index.js
+++ components/MainLayout/index.js
@@ -1,6 +1,7 @@
 import React from "react";
 
 import Head from "../Head";
+import SmallScreenHeader from "./components/SmallScreenHeader";
 import GlobalHeader from "./components/GlobalHeader";
 import PageHeader from "./components/PageHeader";
 import Footer from "./components/Footer";
@@ -8,6 +9,7 @@ import Footer from "./components/Footer";
 const MainLayout = ({ children, route, hideSearchBar, headLinks }) =>
   <div>
     <Head additionalLinks={headLinks} />
+    <SmallScreenHeader />
     <GlobalHeader />
     {!hideSearchBar && <PageHeader searchQuery={route.query.q} />}
     {children}
