a93fa53e694d56be49642407631857e28626b336
Neil Renicker <neilrenicker@gmail.com>
2017-09-12 13:39:25 -0400

    feat: add favicon package

--- components/Head/index.js
+++ components/Head/index.js
@@ -12,7 +12,30 @@ export default ({ additionalLinks }) =>
         name="viewport"
         content="width=device-width, initial-scale=1, shrink-to-fit=no"
       />
-      <meta name="theme-color" content="#000000" />
+      <link
+        rel="apple-touch-icon"
+        sizes="180x180"
+        href="/static/favicons/apple-touch-icon.png"
+      />
+      <link
+        rel="icon"
+        type="image/png"
+        sizes="32x32"
+        href="/static/favicons/favicon-32x32.png"
+      />
+      <link
+        rel="icon"
+        type="image/png"
+        sizes="16x16"
+        href="/static/favicons/favicon-16x16.png"
+      />
+      <link rel="manifest" href="/manifest.json" />
+      <link
+        rel="mask-icon"
+        href="/static/favicons/safari-pinned-tab.svg"
+        color="#5bbad5"
+      />
+      <meta name="theme-color" content="#ffffff" />
       <link
         href="https://fonts.googleapis.com/css?family=Lora:400,400i,700|Source+Sans+Pro:400,400i,600,600i"
         rel="stylesheet"
