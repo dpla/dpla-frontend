899e5f15561aec398624d7cb1d965ef3e98305c4
Frankie Simms <fdsimms@gmail.com>
2017-09-13 17:09:41 -0400

    wip

--- components/MainLayout/components/GlobalHeader/index.js
+++ components/MainLayout/components/GlobalHeader/index.js
@@ -6,20 +6,44 @@ import { stylesheet, classNames } from "./GlobalHeader.css";
 const GlobalHeader = () =>
   <div className={classNames.header}>
     <div className={`${classNames.linksContainer} site-max-width`}>
-      <div className={classNames.links}>
-        <Link href="/">
-          Home
-        </Link>
-        <Link href="/browse-by-topic">
-          Browse
-        </Link>
-        <Link href="/exhibitions">
-          Exhibitions
-        </Link>
-        <Link href="/primary-source-sets" prefetch>
-          Primary Source Sets
-        </Link>
-      </div>
+      <ul className={classNames.links}>
+        <li>
+          <Link href="/">
+            <a>
+              Home
+            </a>
+          </Link>
+        </li>
+        <li className={classNames.browse}>
+          <button className={classNames.browseButton}>Browse</button>
+          <ul className={classNames.browseLinks}>
+            <li>
+              <Link href="/browse-by-topic">
+                <a className={classNames.browseLink}>Browse by Topic</a>
+              </Link>
+            </li>
+            <li>
+              <Link>
+                <a className={classNames.browseLink}>Browse by Partner</a>
+              </Link>
+            </li>
+          </ul>
+        </li>
+        <li>
+          <Link href="/exhibitions">
+            <a>
+              Exhibitions
+            </a>
+          </Link>
+        </li>
+        <li>
+          <Link href="/primary-source-sets">
+            <a>
+              Primary Source Sets
+            </a>
+          </Link>
+        </li>
+      </ul>
       <span className={classNames.divider} />
       <div className={`${classNames.links} ${classNames.secondaryLinks}`}>
         <Link href="">
