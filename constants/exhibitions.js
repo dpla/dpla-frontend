f367d6a7fbe169b610b127c16849987324b434a4
Frankie Simms <fdsimms@gmail.com>
2017-09-25 15:46:46 -0400

    chore: use staging subdomain to test exhibitions API proxy

--- constants/exhibitions.js
+++ constants/exhibitions.js
@@ -2,7 +2,7 @@ export const TITLE = "Exhibitions";
 export const DESCRIPTION = `Stories of national significance drawn from
 source materials in libraries, archives, and museums across the United States.`;
 const BASE_ENDPOINT = process.env.NODE_ENV === "production"
-  ? "http://dp.la/api"
+  ? "http://postlight-staging.dp.la/api"
   : "http://localhost:3000/api";
 export const EXHIBITS_ENDPOINT = `${BASE_ENDPOINT}/exhibitions`;
 export const EXHIBIT_PAGES_ENDPOINT = `${BASE_ENDPOINT}/exhibition_pages`;
