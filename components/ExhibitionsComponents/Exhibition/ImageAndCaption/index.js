cf4be3e8baed0a9deae70c44f751dc22ef094638
Frankie Simms <fdsimms@gmail.com>
2017-09-26 10:59:18 -0400

    fix: proper href for show exhibition link

--- components/ExhibitionsComponents/Exhibition/ImageAndCaption/index.js
+++ components/ExhibitionsComponents/Exhibition/ImageAndCaption/index.js
@@ -33,7 +33,7 @@ const ImageAndCaption = ({ exhibition, route }) =>
                 })
               }}
               as={{
-                pathname: `/${exhibition.slug}/${exhibition.sections[0].slug}/`
+                pathname: `/exhibitions/${exhibition.slug}/${exhibition.sections[0].slug}/`
               }}
             >
               <a className={classNames.exploreLink}>Explore Exhibition</a>
--- server.js
+++ server.js
@@ -207,8 +207,16 @@ app
         pathRewrite: { "^/api/exhibition_pages": "/api/exhibit_pages" }
       })
     );
+    server.get("/api/files/", (req, res, next) => {
+      proxy({
+        target: "http://omeka.internal.dp.la",
+        changeOrigin: true,
+        logLevel: "error"
+      });
+    });
+
     server.get(
-      "/api/files/",
+      "/api/files/{original,thumbnails,square_thumbnails,fullsize}",
       proxy({
         target: "http://omeka.internal.dp.la",
         changeOrigin: true,
