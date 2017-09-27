3203eac6fe90a2db28c7f0b7ff765dc37ade9403
Frankie Simms <fdsimms@gmail.com>
2017-09-27 15:28:36 -0400

    fix: mixed content on homepage (#206)
    * fix: mixed content warning on homepage
    
    * wip: fix slider items collapsing on home page, firefox
    
    * fix: https on all wordpress api calls

--- constants/topicBrowse.js
+++ constants/topicBrowse.js
@@ -4,7 +4,7 @@ across the United States, organized by DPLA's content curation staff into easy
 to navigate sub-topics.`;
 
 export const API_ENDPOINT_ALL_TOPICS =
-  "http://dpla.wpengine.com/wp-json/wp/v2/categories";
+  "https://dpla.wpengine.com/wp-json/wp/v2/categories";
 export const API_ENDPOINT_ALL_TOPICS_100_PER_PAGE = `${API_ENDPOINT_ALL_TOPICS}?per_page=100`;
 export const API_ENDPOINT_ALL_ITEMS =
   "https://dpla.wpengine.com/wp-json/wp/v2/topic_browse_item";
