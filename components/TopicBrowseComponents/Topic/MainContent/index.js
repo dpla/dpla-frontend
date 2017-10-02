56f38d3ec203e126b65a2581d4d1602935fc3ad5
Neil Renicker <neilrenicker@gmail.com>
2017-10-02 12:46:53 -0400

    wip: topic browse card height, responsiveness for landing

--- components/TopicBrowseComponents/Topic/MainContent/index.js
+++ components/TopicBrowseComponents/Topic/MainContent/index.js
@@ -3,39 +3,32 @@ import Link from "next/link";
 
 import { classNames, stylesheet } from "./MainContent.css";
 import { classNames as utilClassNames } from "css/utils.css";
-const { module } = utilClassNames;
 
-const MainContent = ({ topic }) =>
+const MainContent = ({ topic }) => (
   <div className={classNames.wrapper}>
-    <div className={module}>
-      <h1 className={classNames.header}>
-        {topic.name}
-      </h1>
-      <p className={classNames.topicDescription}>
-        {topic.description}
-      </p>
+    <div className={`${classNames.container} site-max-width`}>
+      <h1 className={classNames.header}>{topic.name}</h1>
+      {topic.description && (
+        <p className={classNames.topicDescription}>{topic.description}</p>
+      )}
       <div className={classNames.divider} />
-      <ul className={classNames.subtopics}>
-        {topic.subtopics.map(subtopic =>
-          <li className={classNames.subtopic}>
+      <ul className="row">
+        {topic.subtopics.map(subtopic => (
+          <li className={`${classNames.subtopic} col-xs-12 col-sm-6 col-md-4`}>
             <Link
               prefetch
               as={`/browse-by-topic/${topic.slug}/${subtopic.slug}`}
               href={`/browse-by-topic/topic/subtopic?subtopic=${subtopic.slug}&topic=${topic.slug}`}
             >
               <a className={classNames.subtopicAnchor}>
-                <div
-                  className={classNames.imageWrapper}
-                >
+                <div className={classNames.imageWrapper}>
                   <div
                     className={classNames.image}
                     style={{ backgroundImage: `url(${subtopic.thumbnailUrl})` }}
-                    >
+                  >
                     <div className={classNames.gradientBackground} />
                   </div>
-                  <h3 className={classNames.subtopicTitle}>
-                    {subtopic.name}
-                  </h3>
+                  <h3 className={classNames.subtopicTitle}>{subtopic.name}</h3>
                 </div>
                 <p className={classNames.subtopicDescription}>
                   {subtopic.description}
@@ -43,10 +36,11 @@ const MainContent = ({ topic }) =>
               </a>
             </Link>
           </li>
-        )}
+        ))}
       </ul>
     </div>
     <style dangerouslySetInnerHTML={{ __html: stylesheet }} />
-  </div>;
+  </div>
+);
 
 export default MainContent;
