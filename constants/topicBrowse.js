2f2b4c6985ee233559752d5ec3f0e41d1d7dc79f
mga <mga@ip-192-168-1-9.ec2.internal>
2017-10-10 11:33:29 -0400

    fix: case in browse by topic title

--- constants/topicBrowse.js
+++ constants/topicBrowse.js
@@ -1,4 +1,4 @@
-export const TITLE = "Browse By Topic";
+export const TITLE = "Browse by Topic";
 export const DESCRIPTION = `Materials from libraries, archives and museums
 across the United States, organized by DPLA's content curation staff into easy
 to navigate sub-topics.`;
