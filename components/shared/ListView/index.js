af2625d5b69349cb64800e2e79c380e39f6863f3
Frankie Simms <fdsimms@gmail.com>
2017-10-11 15:02:39 -0400

    styles: polish

--- components/shared/ListView/index.js
+++ components/shared/ListView/index.js
@@ -17,7 +17,7 @@ const ListView = ({ items, route }) =>
     {items.map(item =>
       <li key={item["@id"] || item.id} className={classNames.listItem}>
         <Link prefetch href={item.linkHref} as={item.linkAs}>
-          <a className={classNames.listItemLink}>
+          <a className={`${classNames.listItemLink} ${classNames.listItemImageLink}`}>
             <ListImage
               title={item.title}
               type={item.type}
