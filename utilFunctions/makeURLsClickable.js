aab9ae0a4b5625bcd65dfdb43e6068191f98eec5
Michael Della Bitta <mdellabitta@gmail.com>
2017-10-13 11:53:30 -0400

    Prettier

--- utilFunctions/makeURLsClickable.js
+++ utilFunctions/makeURLsClickable.js
@@ -1,5 +1,5 @@
 // hacky... use only in trusted places
 const makeURLsClickable = (str, className) =>
-  str.replace(/([h|H]ttps?:.+?)(\s|$)/g, (text, link) => '<a class="' + className + '" href="'+ link +'">'+ link +'</a>');
+  str.replace(/([h|H]ttps?:.+?)(\s|$)/g, (text, link) => '<a class="' + className + '" href="' + link + '">' + link + '</a>');
 
 export default makeURLsClickable;
