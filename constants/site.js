32cb23be5ce16545e566fc7be84ed9572992e038
Michael Della Bitta <mdellabitta@gmail.com>
2017-10-13 11:22:23 -0400

    fix: implemented standardized rights statements (#264)

--- constants/site.js
+++ constants/site.js
@@ -15,3 +15,82 @@ export const resourceTypes = {
   SOUND: "sound",
   VIDEO: "moving image"
 };
+
+export const rightsURLs = {
+  "http://rightsstatements.org/vocab/NoC-US/1.0/": { 
+    description: "The organization that has made the Item available believes that the Item is in the Public Domain under the laws of the United States, but a determination was not made as to its copyright status under the copyright laws of other countries. The Item may not be in the Public Domain under the laws of other countries. Please refer to the organization that has made the Item available for more information.",
+    image: ""
+  },
+  "http://rightsstatements.org/vocab/NoC-CR/1.0/": {
+    description: "Use of this Item is not restricted by copyright and/or related rights. As part of the acquisition or digitization of this Item, the organization that has made the Item available is contractually required to limit the use of this Item. Limitations may include, but are not limited to, privacy issues, cultural protections, digitization agreements or donor agreements. Please refer to the organization that has made the Item available for more information.",
+    image: ""
+  },  
+  "http://rightsstatements.org/vocab/NoC-NC/1.0/": {
+    description: "This object has been digitized in a public-private partnership. As part of this partnership, the partners have agreed to limit commercial uses of this digital representation of the object by third parties. You can, without permission, copy, modify, distribute, display, or perform the digital object, for non-commercial uses. For any other permissible uses, please review the terms and conditions of the organization that has made the item available.",
+    image: ""
+  },
+  "http://rightsstatements.org/vocab/NoC-OKLR/1.0/": {
+    description: "Use of this Item is not restricted by copyright and/or related rights. In one or more jurisdictions, laws other than copyright are known to impose restrictions on the use of this Item. Please refer to the organization that has made the Item available for more information.",
+    image: ""
+  },
+  "http://rightsstatements.org/vocab/NKC/1.0/": {
+    description: "The organization that has made the Item available reasonably believes that the Item is not restricted by copyright or related rights, but a conclusive determination could not be made. Please refer to the organization that has made the Item available for more information. You are free to use this Item in any way that is permitted by the copyright and related rights legislation that applies to your use.",
+    image: ""
+  },
+  "http://rightsstatements.org/page/InC/1.0/": {
+    description: "This Item is protected by copyright and/or related rights. You are free to use this Item in any way that is permitted by the copyright and related rights legislation that applies to your use. For other uses you need to obtain permission from the rights-holder(s).a",
+    image: ""
+  },
+  "http://rightsstatements.org/vocab/InC-OW-EU/1.0/": {
+    description: "This Item has been identified as an orphan work in the country of first publication and in line with Directive 2012/28/EU of the European Parliament and of the Council of 25 October 2012 on certain permitted uses of orphan works. For this Item, either (a) no rights-holder(s) have been identified or (b) one or more rights-holder(s) have been identified but none have been located even though a diligent search for the rights-holder(s) has been conducted. The results of the diligent search are available in the EU Orphan Works Database. You are free to use this Item in any way that is permitted by the copyright and related rights legislation that applies to your use.",
+    image: ""
+  },
+  "http://rightsstatements.org/page/InC-EDU/1.0/": {
+    description: "This Item is protected by copyright and/or related rights. You are free to use this Item in any way that is permitted by the copyright and related rights legislation that applies to your use. In addition, no permission is required from the rights-holder(s) for educational uses. For other uses, you need to obtain permission from the rights-holder(s).",
+    image: ""
+  },
+  "http://rightsstatements.org/page/InC-NC/1.0/": {
+    description: "This Item is protected by copyright and/or related rights. You are free to use this Item in any way that is permitted by the copyright and related rights legislation that applies to your use. In addition, no permission is required from the rights-holder(s) for non-commercial uses. For other uses you need to obtain permission from the rights-holder(s).",
+    image: ""
+  },
+  "http://rightsstatements.org/page/InC-RUU/1.0/": {
+    description: "This Item is protected by copyright and/or related rights. However, for this Item, either (a) no rights-holder(s) have been identified or (b) one or more rights-holder(s) have been identified but none have been located. You are free to use this Item in any way that is permitted by the copyright and related rights legislation that applies to your use.",
+    image: ""
+  },
+  "http://rightsstatements.org/vocab/CNE/1.0/": {
+    description: "This Rights Statement should be used for Items for which the copyright status is unknown and for which the data provider has not undertaken an effort to determine the copyright status of the work.",
+    image: ""
+  },
+  "http://rightsstatements.org/vocab/UND/1.0/": {
+    description: "This Rights Statement should be used for Items for which the copyright status is unknown and for which the organization that has made the Item available has undertaken an effort to determine the copyright status of the Work. Typically, this Rights Statement is used when the organization is missing key facts essential to making an accurate copyright status determination.",
+    image: ""
+  },  
+  "https://creativecommons.org/licenses/by/4.0/": {
+    description: "",
+    image: ""
+  },
+  "https://creativecommons.org/licenses/by-nc-nd/4.0/": {
+    description: "",
+    image: ""
+  },
+  "https://creativecommons.org/licenses/by-sa/4.0/":  {
+    description: "",
+    image: ""
+  },
+  "https://creativecommons.org/licenses/by-nd/4.0/": {
+    description: "",
+    image: ""
+  },
+  "https://creativecommons.org/licenses/by-nc/4.0/": {
+    description: "",
+    image: ""
+  },
+  "https://creativecommons.org/licenses/by-nc-sa/4.0/": {
+    description: "",
+    image: ""
+  },
+  "https://creativecommons.org/publicdomain/zero/1.0/": {
+    description: "",
+    image: ""
+  }
+};
