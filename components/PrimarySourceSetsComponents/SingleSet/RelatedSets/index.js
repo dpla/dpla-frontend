31e70ee6d6065441efdf226cd1f7a2a570e5f572
Neil Renicker <neilrenicker@gmail.com>
2017-10-16 12:17:26 -0400

    feat: hook up and style related PSS (#277)

--- components/PrimarySourceSetsComponents/SingleSet/RelatedSets/index.js
+++ components/PrimarySourceSetsComponents/SingleSet/RelatedSets/index.js
@@ -1,30 +1,46 @@
 import React from "react";
+import Link from "next/link";
 
 import { stylesheet, classNames } from "./RelatedSets.css";
 import { classNames as utilClassNames } from "css/utils.css";
+import { extractSourceSetSlug } from "utilFunctions/";
 
 const { container } = utilClassNames;
 
-const RelatedSets = ({ sets }) =>
-  <div className={classNames.wrapper}>
-    <div className={[container, classNames.relatedSets].join(" ")}>
-      <h3 className={classNames.header}>Related Primary Source Sets</h3>
-      <div className={classNames.sets}>
-        {sets.map(({ img, name, thumbnailUrl }) =>
-          <div key={name} className={classNames.set}>
-            <img
-              alt={name}
-              src={thumbnailUrl}
-              className={classNames.setImage}
-            />
-            <p className={classNames.title}>
-              {name}
-            </p>
-          </div>
-        )}
+const RelatedSets = ({ sets }) => {
+  return (
+    <div className={classNames.wrapper}>
+      <div className={[container, classNames.relatedSets].join(" ")}>
+        <h3 className={classNames.header}>Related Primary Source Sets</h3>
+        <div className="row">
+          {sets.map((set, index) =>
+            <div
+              key={`${set.name}-${index}`}
+              className="col-12 col-xs-6 col-md-3"
+            >
+              <Link
+                prefetch
+                href={`/primary-source-sets/set?set=${extractSourceSetSlug(
+                  set["@id"]
+                )}`}
+                as={`/primary-source-sets/${extractSourceSetSlug(set["@id"])}`}
+              >
+                <a className={classNames.setLink}>
+                  <img
+                    alt={set.name}
+                    src={set.repImageUrl || set.thumbnailUrl}
+                    className={classNames.setImage}
+                  />
+                  <p className={classNames.title}>{set.name}</p>
+                </a>
+              </Link>
+            </div>
+          )}
+        </div>
       </div>
+      <style dangerouslySetInnerHTML={{ __html: stylesheet }} />
     </div>
-    <style dangerouslySetInnerHTML={{ __html: stylesheet }} />
-  </div>;
+  );
+};
 
 export default RelatedSets;
