59a832e36985f2e9d81ddbb1ac9f82cdfb14c473
Frankie Simms <fdsimms@gmail.com>
2017-10-19 14:36:00 -0400

    wip

--- components/ItemComponents/BreadcrumbsModule/index.js
+++ components/ItemComponents/BreadcrumbsModule/index.js
@@ -1,14 +1,99 @@
 import React from "react";
-const markdownit = require("markdown-it")({ html: true });
+import Link from "next/link";
 
 import Breadcrumbs from "../../Breadcrumbs";
 
 import { stylesheet, classNames } from "./BreadcrumbsModule.css";
 import { classNames as utilClassNames } from "css/utils.css";
+const markdownit = require("markdown-it")({ html: true });
 
 const { container } = utilClassNames;
 
-const BreadcrumbsModule = ({ route, breadcrumbs }) =>
+const PreviousItemLink = ({
+  query,
+  searchItemCount,
+  previousItemId,
+  nextItemId
+}) => {
+  const previous = parseInt(query.previous, 10);
+  const next = parseInt(query.next, 10);
+  const page_size = parseInt(query.page_size, 10);
+  const page = parseInt(query.page, 10);
+  const newPage = page > 0 && previous === -1 ? page - 1 : page;
+  console.log(newPage);
+  const newPrevious = previous > 0
+    ? previous - 1
+    : newPage !== page ? page_size - 1 : "";
+  return (
+    <Link
+      href={{
+        pathname: "/item",
+        query: {
+          ...query,
+          itemId: previousItemId,
+          previous: newPrevious,
+          next: next && next < searchItemCount - 1 ? next - 1 : "",
+          page: newPage
+        }
+      }}
+      as={{
+        pathname: `/item/${previousItemId}`,
+        query: {
+          ...query,
+          previous: previous > 0 && previous >= 0 ? previous - 1 : "",
+          next: next && next < searchItemCount - 1 ? next - 1 : ""
+        }
+      }}
+    >
+      <a>
+        Previous
+      </a>
+    </Link>
+  );
+};
+const NextItemLink = ({
+  query,
+  searchItemCount,
+  previousItemId,
+  nextItemId
+}) => {
+  const { next, previous } = query;
+  const previousIdx = parseInt(previous, 10);
+  const nextIdx = parseInt(next, 10);
+  return (
+    <Link
+      href={{
+        pathname: "/item",
+        query: {
+          ...query,
+          itemId: nextItemId,
+          previous: previousIdx > 0 && previousIdx >= 0 ? previousIdx - 1 : "",
+          next: nextIdx && nextIdx < searchItemCount - 1 ? nextIdx - 1 : ""
+        }
+      }}
+      as={{
+        pathname: `/item/${nextItemId}`,
+        query: {
+          ...query,
+          previous: previousIdx > 0 && previousIdx >= 0 ? previousIdx - 1 : "",
+          next: nextIdx && nextIdx < searchItemCount - 1 ? nextIdx - 1 : ""
+        }
+      }}
+    >
+      <a>
+        Next
+      </a>
+    </Link>
+  );
+};
+
+const BreadcrumbsModule = ({
+  route,
+  breadcrumbs,
+  nextItemId,
+  previousItemId,
+  searchItemCount
+}) =>
   <div className={classNames.wrapper}>
     <div className={[container, classNames.breadcrumbsModule].join(" ")}>
       <Breadcrumbs
@@ -17,6 +102,18 @@ const BreadcrumbsModule = ({ route, breadcrumbs }) =>
             title: markdownit.render(breadcrumb.title)
           })
         )}
+      />
+      <PreviousItemLink
+        query={route.query}
+        searchItemCount={searchItemCount}
+        previousItemId={previousItemId}
+        nextItemId={nextItemId}
+      />
+      <NextItemLink
+        query={route.query}
+        searchItemCount={searchItemCount}
+        previousItemId={previousItemId}
+        nextItemId={nextItemId}
       />
     </div>
     <style dangerouslySetInnerHTML={{ __html: stylesheet }} />
--- pages/item/index.js
+++ pages/item/index.js
@@ -9,6 +9,12 @@ import ShareButton from "components/shared/ShareButton";
 import { API_ENDPOINT, THUMBNAIL_ENDPOINT } from "constants/items";
 import { getCurrentUrl, getCurrentFullUrl } from "utilFunctions";
 import { classNames as utilClassNames } from "css/utils.css";
+import { DEFAULT_PAGE_SIZE } from "constants/search";
+import {
+  possibleFacets,
+  mapFacetsToURLPrettified,
+  splitAndURIEncodeFacet
+} from "constants/search";
 import {
   classNames,
   stylesheet
@@ -19,9 +25,19 @@ import {
   getDefaultThumbnail
 } from "utilFunctions";
 
-const ItemDetail = ({ url, item, currentFullUrl }) =>
+const ItemDetail = ({
+  url,
+  item,
+  currentFullUrl,
+  previousItemId,
+  nextItemId,
+  searchItemCount
+}) =>
   <MainLayout route={url} pageTitle={item.title}>
     <BreadcrumbsModule
+      searchItemCount={searchItemCount}
+      previousItemId={previousItemId}
+      nextItemId={nextItemId}
       breadcrumbs={[
         {
           title: url.query.q ? `Search for “${url.query.q}”` : "Search",
@@ -52,6 +68,7 @@ const ItemDetail = ({ url, item, currentFullUrl }) =>
 ItemDetail.getInitialProps = async ({ query, req }) => {
   const currentFullUrl = getCurrentFullUrl(req);
   const currentUrl = getCurrentUrl(req);
+
   const res = await fetch(`${currentUrl}${API_ENDPOINT}/${query.itemId}`);
   const json = await res.json();
   const doc = json.docs[0];
@@ -67,8 +84,50 @@ ItemDetail.getInitialProps = async ({ query, req }) => {
         return lang.name;
       })
     : doc.sourceResource.language;
+
+  let previousItemId;
+  let nextItemId;
+  let searchItemCount;
+  if (query.q && (query.next || query.previous)) {
+    const q = query.q;
+    const page_size = query.page_size || DEFAULT_PAGE_SIZE;
+    const page = query.previous === -1 ? parseInt(query.page, 10) - 1 : (query.page || 1);
+    let sort_by = "";
+    if (query.sort_by === "title") {
+      sort_by = "sourceResource.title";
+    } else if (query.sort_by === "created") {
+      sort_by = "sourceResource.date.begin";
+    }
+    const sort_order = query.sort_order || "";
+
+    const facetQueries = possibleFacets
+      .map(
+        facet =>
+          query[mapFacetsToURLPrettified[facet]]
+            ? `${facet}=${splitAndURIEncodeFacet(
+                query[mapFacetsToURLPrettified[facet]]
+              )}`
+            : ""
+      )
+      .filter(facetQuery => facetQuery !== "")
+      .join("&");
+
+    const searchRes = await fetch(
+      `${currentUrl}${API_ENDPOINT}?q=${q}&page=${page}&page_size=${page_size}&sort_order=${sort_order}&sort_by=${sort_by}&facets=${possibleFacets.join(
+        ","
+      )}&${facetQueries}`
+    );
+    const searchJson = await searchRes.json();
+    previousItemId = query.previous ? searchJson.docs[query.previous].id : "";
+    nextItemId = query.next ? searchJson.docs[query.next].id : "";
+    searchItemCount = searchJson.docs.length;
+  }
+
   return {
     currentFullUrl,
+    previousItemId,
+    nextItemId,
+    searchItemCount,
     item: Object.assign({}, doc.sourceResource, {
       thumbnailUrl,
       contributor: doc.dataProvider,
