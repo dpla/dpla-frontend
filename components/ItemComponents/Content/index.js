5726c821e8f88db347073b5ca5dab8556e129cd5
AudreyAltman <AudreyAltman@users.noreply.github.com>
2017-10-20 11:45:32 -0400

    add schema.org item markup (#295)
    * add schema.org item markup

--- components/ItemComponents/Content/index.js
+++ components/ItemComponents/Content/index.js
@@ -4,13 +4,15 @@ import { classNames, stylesheet } from "./Content.css";
 import MainMetadata from "./MainMetadata";
 import OtherMetadata from "./OtherMetadata";
 import RightsMetadata from "./RightsMetadata";
+import JsonLdMarkup from "./JsonLdMarkup";
 
-const Content = ({ item }) =>
+const Content = ({ item, url }) =>
   <div className={classNames.content}>
     <MainMetadata item={item} />
     <OtherMetadata item={item} />
     <RightsMetadata item={item} />
     <style dangerouslySetInnerHTML={{ __html: stylesheet }} />
+    <JsonLdMarkup item={item} url={url} />
   </div>;
 
 export default Content;
