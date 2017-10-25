b64e39c43cdfa35f7774b9a576113b58fa89d16f
Frankie Simms <fdsimms@gmail.com>
2017-10-25 10:32:26 -0400

    fix: remove duped markdownit import

--- pages/primary-source-sets/set/additional-resources.js
+++ pages/primary-source-sets/set/additional-resources.js
@@ -1,6 +1,5 @@
 import React from "react";
 import fetch from "isomorphic-fetch";
-const markdownit = require("markdown-it")({ html: true });
 
 import MainLayout from "/components/MainLayout";
 import PSSFooter from "/components/PrimarySourceSetsComponents/PSSFooter";
