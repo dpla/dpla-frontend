cd931cab8680ce98d0c19840ede138c5252db881
Mauricio Giraldo Arteaga <mauricio@pingpongestudio.com>
2017-11-16 11:57:56 -0500

    :space_invader: fixed topic browse in mobile / a11y isssues

--- components/TopicBrowseComponents/Topic/MainContent/index.js
+++ components/TopicBrowseComponents/Topic/MainContent/index.js
@@ -5,15 +5,16 @@ import HeadingRule from "components/shared/HeadingRule";
 import { classNames, stylesheet } from "./MainContent.css";
 import { classNames as utilClassNames } from "css/utils.css";
 
-const MainContent = ({ topic }) =>
+const MainContent = ({ topic }) => (
   <div className={classNames.wrapper}>
     <div className={`${classNames.container} site-max-width`}>
       <h1 className={classNames.header}>{topic.name}</h1>
-      {topic.description &&
-        <p className={classNames.topicDescription}>{topic.description}</p>}
+      {topic.description && (
+        <p className={classNames.topicDescription}>{topic.description}</p>
+      )}
       <HeadingRule color="#F9BA3F" />
       <ul className="row">
-        {topic.subtopics.map((subtopic, index) =>
+        {topic.subtopics.map((subtopic, index) => (
           <li
             className={`${classNames.subtopic} col-xs-12 col-sm-6 col-md-4`}
             key={`${subtopic.name}-${index}`}
@@ -21,28 +22,30 @@ const MainContent = ({ topic }) =>
             <Link
               prefetch
               as={`/browse-by-topic/${topic.slug}/${subtopic.slug}`}
-              href={`/browse-by-topic/topic/subtopic?subtopic=${subtopic.slug}&topic=${topic.slug}`}
+              href={`/browse-by-topic/topic/subtopic?subtopic=${
+                subtopic.slug
+              }&topic=${topic.slug}`}
             >
               <a className={classNames.subtopicAnchor}>
-                <div className={classNames.imageWrapper}>
-                  <div
-                    className={classNames.image}
-                    style={{ backgroundImage: `url(${subtopic.thumbnailUrl})` }}
-                  >
-                    <div className={classNames.gradientBackground} />
-                  </div>
+                <img
+                  className={classNames.image}
+                  aria-hidden="true"
+                  src={subtopic.thumbnailUrl}
+                />
+                <div className={classNames.textWrapper}>
                   <h3 className={classNames.subtopicTitle}>{subtopic.name}</h3>
+                  <p className={classNames.subtopicDescription}>
+                    {subtopic.description}
+                  </p>
                 </div>
-                <p className={classNames.subtopicDescription}>
-                  {subtopic.description}
-                </p>
               </a>
             </Link>
           </li>
-        )}
+        ))}
       </ul>
     </div>
     <style dangerouslySetInnerHTML={{ __html: stylesheet }} />
-  </div>;
+  </div>
+);
 
 export default MainContent;
