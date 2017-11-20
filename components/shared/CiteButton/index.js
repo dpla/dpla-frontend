bb25f60c389a15efa2ed8cacab5614daf2b41513
Mauricio Giraldo Arteaga <mauricio@pingpongestudio.com>
2017-11-20 10:02:42 -0500

    :space_invader: added intl import to cite button

--- components/shared/CiteButton/index.js
+++ components/shared/CiteButton/index.js
@@ -1,5 +1,7 @@
 import React from "react";
 import Button from "components/shared/Button";
+import Intl from "intl";
+import en from "intl/locale-data/jsonp/en";
 
 import { joinIfArray, removeEndPunctuation } from "utilFunctions";
 
