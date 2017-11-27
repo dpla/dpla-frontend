88427f45d9e60bd01c8b83a25d9ad34ac6dfe869
Mauricio Giraldo <mgiraldo@users.noreply.github.com>
2017-11-27 13:32:34 -0500

    :space_invader: added nil check for query.q in search (#392)

--- pages/search/index.js
+++ pages/search/index.js
@@ -69,10 +69,9 @@ class Search extends React.Component {
 
 Search.getInitialProps = async ({ query, req }) => {
   const currentUrl = getCurrentUrl(req);
-  const q =
-    encodeURIComponent(query.q)
-      .replace(/'/g, "%27")
-      .replace(/"/g, "%22") || "";
+  const q = query.q
+    ? encodeURIComponent(query.q).replace(/'/g, "%27").replace(/"/g, "%22")
+    : "";
   const page_size = query.page_size || DEFAULT_PAGE_SIZE;
   const page = query.page || 1;
   let sort_by = "";
@@ -96,9 +95,7 @@ Search.getInitialProps = async ({ query, req }) => {
     .join("&");
 
   const res = await fetch(
-    `${currentUrl}${API_ENDPOINT}?q=${q}&page=${page}&page_size=${
-      page_size
-    }&sort_order=${sort_order}&sort_by=${sort_by}&facets=${possibleFacets.join(
+    `${currentUrl}${API_ENDPOINT}?q=${q}&page=${page}&page_size=${page_size}&sort_order=${sort_order}&sort_by=${sort_by}&facets=${possibleFacets.join(
       ","
     )}&${facetQueries}`
   );
