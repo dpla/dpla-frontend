35a8ffae6d8bbd41de954f6884170bbe12f26ae4
Michael Della Bitta <mdellabitta@gmail.com>
2017-12-05 17:01:03 -0500

    fixes DT-1939

--- pages/donate/index.js
+++ pages/donate/index.js
@@ -57,7 +57,7 @@ const Donate = ({ url }) =>
                 srcset="/static/images/adena_lo.jpg,
                         /static/images/adena_hi.jpg 2x"
                 src="/static/images/adena_lo.jpg"
-                alt="Adena, a high school school teacher in West Virginia"
+                alt="Adena, a high school school teacher in West Virginia, leads her students through a demonstration of DPLA's web site"
               />
               <span>Adena, a high school school teacher in West Virginia</span>
             </div>
