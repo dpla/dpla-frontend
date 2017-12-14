46a256d1707db67bf11d7ea55f27fd82e3ebd836
Mauricio Giraldo Arteaga <mauricio@pingpongestudio.com>
2017-12-14 15:02:46 -0500

    :space_invader: cite in mobile is after text / repeat explore exhibit after text

--- components/ExhibitionsComponents/Exhibition/Details/index.js
+++ components/ExhibitionsComponents/Exhibition/Details/index.js
@@ -1,9 +1,11 @@
 import React from "react";
 import Link from "next/link";
 
+import Button from "../../../shared/Button";
+import CiteButton from "../../../shared/CiteButton";
+
 import { classNames, stylesheet } from "./Details.css";
 import { classNames as utilClassNames } from "css/utils.css";
-import CiteButton from "components/shared/CiteButton";
 
 const { container } = utilClassNames;
 const Details = ({ exhibition, route, currentFullUrl }) =>
@@ -49,14 +51,44 @@ const Details = ({ exhibition, route, currentFullUrl }) =>
         </div>
       </div>
       <div className={classNames.body}>
-        <span
+        <div
           className={classNames.bodyText}
           dangerouslySetInnerHTML={{ __html: exhibition.text }}
         />
+        <div className={classNames.exploreButton}>
+          <Button
+            type="primary"
+            size="large"
+            prefetch
+            className={classNames.exploreLink}
+            url={{
+              pathname: "/exhibitions/exhibition/section/subsection",
+              query: Object.assign({}, route.query, {
+                section: exhibition.sections[0].slug,
+                exhibition: exhibition.slug,
+                subsection: ""
+              })
+            }}
+            as={{
+              pathname: `/exhibitions/${exhibition.slug}/${exhibition
+                .sections[0].slug}/`
+            }}
+          >
+            Explore Exhibition
+          </Button>
+        </div>
         <p className={classNames.credits}>
           <span className={classNames.creditsBold}>Credit: </span>
           <span>{exhibition.credits}</span>
         </p>
+        <div className={classNames.faveAndCiteButtonsWrapper}>
+          <CiteButton
+            toCiteText="exhibition"
+            className={classNames.citeButton}
+            freeText={exhibition.description}
+            title={exhibition.name}
+          />
+        </div>
       </div>
     </div>
     <style dangerouslySetInnerHTML={{ __html: stylesheet }} />
--- components/ExhibitionsComponents/Exhibition/ImageAndCaption/index.js
+++ components/ExhibitionsComponents/Exhibition/ImageAndCaption/index.js
@@ -1,7 +1,7 @@
 import React from "react";
 
-import HeadingRule from "components/shared/HeadingRule";
-import Button from "components/shared/Button";
+import HeadingRule from "../../../shared/HeadingRule";
+import Button from "../../../shared/Button";
 import { classNames, stylesheet } from "./ImageAndCaption.css";
 import { classNames as utilClassNames } from "css/utils.css";
 
