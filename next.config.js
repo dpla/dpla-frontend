b7a94bd73daed69207bc11469c8206c7d7e86250
Mauricio Giraldo Arteaga <mauricio@pingpongestudio.com>
2018-01-02 21:16:46 -0500

    :space_invader: disable babel cache

--- next.config.js
+++ next.config.js
@@ -55,6 +55,14 @@ module.exports = {
       ]
     });
 
+    // Perform customizations to config
+    config.module.rules = config.module.rules.map(rule => {
+      if (rule.loader === "babel-loader") {
+        rule.options.cacheDirectory = false;
+      }
+      return rule;
+    });
+
     return config;
   }
 };
