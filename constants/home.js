1a3e66fa9c2e3d24911d9fa3b8873fbfe9a39a41
Mauricio Giraldo Arteaga <mauricio@pingpongestudio.com>
2018-01-08 11:09:03 -0500

    :space_invader: started making wp a env var

--- constants/home.js
+++ constants/home.js
@@ -1,5 +1,6 @@
-export const DPLA_HOMEPAGE_ENDPOINT =
-  "https://dpla.wpengine.com/wp-json/wp/v2/pages/3777";
+import { WORDPRESS_URL } from "./env";
+
+export const DPLA_HOMEPAGE_ENDPOINT = `${WORDPRESS_URL}/wp-json/wp/v2/pages/3777`;
 
 export const NUM_HOMEPAGE_EXHIBITIONS = 8;
 export const HEADER_DESCRIPTION_TOKEN = "%ITEM_COUNT%";
--- constants/topicBrowse.js
+++ constants/topicBrowse.js
@@ -1,9 +1,9 @@
+import { WORDPRESS_URL } from "./env";
+
 export const TITLE = "Browse by Topic";
 export const DESCRIPTION = `Highlights of collections from libraries, archives and museums across the United States, organized into easy-to-navigate topics.`;
 
-export const API_ENDPOINT_ALL_TOPICS =
-  "https://dpla.wpengine.com/wp-json/wp/v2/categories";
+export const API_ENDPOINT_ALL_TOPICS = `${WORDPRESS_URL}/wp-json/wp/v2/categories`;
 export const API_ENDPOINT_ALL_TOPICS_100_PER_PAGE = `${API_ENDPOINT_ALL_TOPICS}?per_page=100`;
-export const API_ENDPOINT_ALL_ITEMS =
-  "https://dpla.wpengine.com/wp-json/wp/v2/topic_browse_item";
+export const API_ENDPOINT_ALL_ITEMS = `${WORDPRESS_URL}/wp-json/wp/v2/topic_browse_item`;
 export const API_ENDPOINT_ALL_ITEMS_100_PER_PAGE = `${API_ENDPOINT_ALL_ITEMS}?per_page=100`;
