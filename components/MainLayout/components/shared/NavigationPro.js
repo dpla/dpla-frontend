46554379565e1795402729a467989b35d3c53138
Mauricio Giraldo Arteaga <mauricio@pingpongestudio.com>
2018-01-11 11:53:21 -0500

    :space_invader: fixed pro main nav

--- components/MainLayout/components/shared/NavigationPro.js
+++ components/MainLayout/components/shared/NavigationPro.js
@@ -5,24 +5,24 @@ const NavigationPro = ({ className, style, classNames }) =>
   <div className={className} style={style}>
     <ul className={classNames.links}>
       <li>
-        <Link prefetch href="/">
+        <Link prefetch as="/" href="/pro">
           <a>
             Home
           </a>
         </Link>
       </li>
       <li>
-        <Link prefetch as="/hubs" href="/wp?section=hubs">
+        <Link prefetch as="/hubs" href="/pro/wp?section=hubs">
           <a>Hubs</a>
         </Link>
       </li>
       <li>
-        <Link prefetch as="/projects" href="/wp?section=projects">
+        <Link prefetch as="/projects" href="/pro/wp?section=projects">
           <a>Projects</a>
         </Link>
       </li>
       <li>
-        <Link prefetch as="/education" href="/wp?section=education">
+        <Link prefetch as="/education" href="/pro/wp?section=education">
           <a>Education</a>
         </Link>
       </li>
@@ -30,17 +30,17 @@ const NavigationPro = ({ className, style, classNames }) =>
     <span className={classNames.divider} />
     <ul className={`${classNames.links} ${classNames.secondaryLinks}`}>
       <li>
-        <Link prefetch as="/events" href="/wp?section=events">
+        <Link prefetch as="/events" href="/pro/wp?section=events">
           <a>Events</a>
         </Link>
       </li>
       <li>
-        <Link prefetch as="/about" href="/wp?section=about-dpla-pro">
+        <Link prefetch as="/about" href="/pro/wp?section=about-dpla-pro">
           <a>About</a>
         </Link>
       </li>
       <li>
-        <Link prefetch as="/news" href="/wp?section=news">
+        <Link prefetch as="/news" href="/pro/wp?section=news">
           <a>News</a>
         </Link>
       </li>
@@ -48,7 +48,7 @@ const NavigationPro = ({ className, style, classNames }) =>
     <span className={classNames.divider} />
     <ul className={`${classNames.links} ${classNames.tertiaryLinks}`}>
       <li>
-        <Link href="//dp.la">
+        <Link href="//beta.dp.la">
           <a>DPLA Main Site</a>
         </Link>
       </li>
