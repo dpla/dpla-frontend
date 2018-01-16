f335f2b388bc5d90d442084490e9ee49745d65cf
Mauricio Giraldo Arteaga <mauricio@pingpongestudio.com>
2018-01-16 18:16:01 -0500

    :space_invader: proper import

--- components/HomePageComponents/FromTheBlog/index.js
+++ components/HomePageComponents/FromTheBlog/index.js
@@ -1,7 +1,7 @@
 import React from "react";
 import Link from "next/link";
 
-import formatDate from "utilFunctions";
+import { formatDate } from "utilFunctions";
 
 import { stylesheet, classNames } from "./FromTheBlog.css";
 
