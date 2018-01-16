a1cf600f946b3239f896c0bd617e33d957c6cd9e
Mauricio Giraldo Arteaga <mauricio@pingpongestudio.com>
2018-01-16 17:51:16 -0500

    :space_invader: rfooter split into pro/user

---
+++ components/MainLayout/components/Footer/FooterPro.js
@@ -0,0 +1,55 @@
+import React from "react";
+import Link from "next/link";
+
+import Button from "components/shared/Button";
+
+import { SECTIONS as PRO_SECTIONS } from "constants/pro";
+
+import { classNames, stylesheet } from "./Footer.css";
+
+const FooterPro = () =>
+  <div className={`${classNames.footer} site-max-width`}>
+    <div className="row">
+      <div className={`${classNames.linkGroup} col-xs-12 col-sm-6 col-md-6`}>
+        <h2 className={classNames.linkHeader}>DPLA Pro</h2>
+        <ul>
+          {PRO_SECTIONS.map((section, index) => {
+            return (
+              <li key={index}>
+                <Link href={`/${section.slug}`}>
+                  <a>{section.name}</a>
+                </Link>
+              </li>
+            );
+          })}
+          <li>
+            <Link href="/news">
+              <a>News</a>
+            </Link>
+          </li>
+        </ul>
+      </div>
+      <div className={`${classNames.linkGroup} col-xs-6 col-sm-6 col-md-3`}>
+        <h2 className={classNames.linkHeader}>DPLA</h2>
+        <ul>
+          <li>
+            <Link href="//dp.la">
+              <a>DPLA Main Site</a>
+            </Link>
+          </li>
+        </ul>
+      </div>
+      <div className="col-xs-12 col-md-3">
+        <Button
+          type="primary"
+          size="large"
+          className={classNames.donateButton}
+          url="/donate"
+        >
+          Donate
+        </Button>
+      </div>
+    </div>
+  </div>;
+
+export default FooterPro;
---
+++ components/MainLayout/components/Footer/FooterUser.js
@@ -0,0 +1,104 @@
+import React from "react";
+import Link from "next/link";
+
+import Button from "components/shared/Button";
+
+import { classNames, stylesheet } from "./Footer.css";
+
+const FooterUser = () =>
+  <div className={`${classNames.footer} site-max-width`}>
+    <div className="row">
+      <div className={`${classNames.linkGroup} col-xs-6 col-sm-4 col-md-3`}>
+        <h2 className={classNames.linkHeader}>DPLA</h2>
+        <ul>
+          <li>
+            <Link href="/about/frequently-asked-questions">
+              <a>Frequently Asked Questions</a>
+            </Link>
+          </li>
+          <li>
+            <Link href="/guides">
+              <a>How Can I Use DPLA?</a>
+            </Link>
+          </li>
+          <li>
+            <Link href="/about/terms-conditions">
+              <a>Terms &amp; Conditions</a>
+            </Link>
+          </li>
+          <li>
+            <Link href="/about/our-supporters">
+              <a>Our Supporters</a>
+            </Link>
+          </li>
+          <li>
+            <Link href="/about">
+              <a>About DPLA</a>
+            </Link>
+          </li>
+          <li>
+            <Link href="/contact">
+              <a>Contact Us</a>
+            </Link>
+          </li>
+          <li>
+            <Link href="/news">
+              <a>News</a>
+            </Link>
+          </li>
+        </ul>
+      </div>
+      <div className={`${classNames.linkGroup} col-xs-6 col-sm-4 col-md-3`}>
+        <h2 className={classNames.linkHeader}>Tools</h2>
+        <ul>
+          <li>
+            <Link href="/primary-source-sets">
+              <a>Primary Source Sets</a>
+            </Link>
+          </li>
+          <li>
+            <Link href="/browse-by-partner">
+              <a>Browse by Partner</a>
+            </Link>
+          </li>
+          <li>
+            <Link href="/browse-by-topic">
+              <a>Browse by Topic</a>
+            </Link>
+          </li>
+          <li>
+            <Link href="/exhibitions">
+              <a>Exhibitions</a>
+            </Link>
+          </li>
+          <li>
+            <Link href="/search">
+              <a>Search</a>
+            </Link>
+          </li>
+        </ul>
+      </div>
+      <div className={`${classNames.linkGroup} col-xs-6 col-sm-4 col-md-3`}>
+        <h2 className={classNames.linkHeader}>DPLA Pro</h2>
+        <ul>
+          <li>
+            <Link href="//pro.dp.la">
+              <a>DPLA Pro Community Site</a>
+            </Link>
+          </li>
+        </ul>
+      </div>
+      <div className="col-xs-12 col-md-3">
+        <Button
+          type="primary"
+          size="large"
+          className={classNames.donateButton}
+          url="/donate"
+        >
+          Donate
+        </Button>
+      </div>
+    </div>
+  </div>;
+
+export default FooterUser;
