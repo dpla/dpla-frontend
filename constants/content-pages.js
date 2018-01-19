d39c302783cdeec2017e39c808afff1623a1bf4c
Mauricio Giraldo Arteaga <mauricio@pingpongestudio.com>
2018-01-19 16:57:51 -0500

    :space_invader: news == blogs / some type love in content pages

--- constants/content-pages.js
+++ constants/content-pages.js
@@ -3,8 +3,7 @@ import { WORDPRESS_URL } from "./env";
 export const ABOUT_MENU_ENDPOINT = `${WORDPRESS_URL}/wp-json/menus/v1/menus/about-us`;
 export const PRO_MENU_ENDPOINT = `${WORDPRESS_URL}/wp-json/menus/v1/menus/pro-site`;
 export const GUIDES_ENDPOINT = `${WORDPRESS_URL}/wp-json/wp/v2/pages/2394`;
-export const NEWS_ENDPOINT = `${WORDPRESS_URL}/wp-json/wp/v2/news`;
-export const BLOGS_ENDPOINT = `${WORDPRESS_URL}/wp-json/wp/v2/posts`;
-export const NEWS_HOME_ENDPOINT = `${BLOGS_ENDPOINT}?per_page=3`;
+export const NEWS_ENDPOINT = `${WORDPRESS_URL}/wp-json/wp/v2/posts`;
+export const NEWS_HOME_ENDPOINT = `${NEWS_ENDPOINT}?per_page=3`;
 export const GUIDES_PARENT_ID = 5325; // the post id of “How Can I Use DPLA?” (which is different from the page id above)
 export const SEO_TYPE = "article";
