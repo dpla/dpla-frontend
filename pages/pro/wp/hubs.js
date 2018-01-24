987b721503cc85ac3048cdacc60539b38ba452ba
Mauricio Giraldo Arteaga <mauricio@pingpongestudio.com>
2018-01-24 16:06:11 -0500

    :space_invader: started styling hub page

--- pages/pro/wp/hubs.js
+++ pages/pro/wp/hubs.js
@@ -17,22 +17,37 @@ import {
   stylesheet as contentStyles
 } from "css/pages/content-pages-wysiwyg.css";
 import { classNames as utilClassNames } from "css/utils.css";
+import { stylesheet, classNames } from "css/pages/hubs.css";
 
-const HubsPage = ({ url, page, items, pageTitle }) =>
+const HubsPage = ({ url, page, items, children, pageTitle }) =>
   <MainLayout route={url} pageTitle={pageTitle} seoType={SEO_TYPE}>
     <div
-      className={`${utilClassNames.container}
-      ${contentClasses.sidebarAndContentWrapper}`}
+      className={`${contentClasses.sidebarAndContentWrapper} ${classNames.pageWrapper}`}
     >
       <div className="row">
-        <IconComponent name="network" />
-        <div id="main" className={contentClasses.content}>
-          <h1>{page.title.rendered}</h1>
-          <div dangerouslySetInnerHTML={{ __html: page.content.rendered }} />
+        <div
+          id="main"
+          className={`${classNames.sectionWrapper} site-max-width`}
+        >
+          <section>
+            <IconComponent name="network" />
+            <h1>{page.title.rendered}</h1>
+            <ul>
+              {children.map((child, index) => {
+                return (
+                  <li key={index} className={classNames.subsection}>
+                    <h2>{child.post_title}</h2>
+                  </li>
+                );
+              })}
+            </ul>
+            <div dangerouslySetInnerHTML={{ __html: page.content.rendered }} />
+          </section>
         </div>
       </div>
     </div>
     <style dangerouslySetInnerHTML={{ __html: contentStyles }} />
+    <style dangerouslySetInnerHTML={{ __html: stylesheet }} />
   </MainLayout>;
 
 HubsPage.getInitialProps = async ({ req, query, res }) => {
