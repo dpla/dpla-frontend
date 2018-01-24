3ab8938c5ebf61826e9aac41686e4275e944feef
Audrey Altman <audrey@dp.la>
2018-01-24 16:26:21 -0500

    remove initGa alert

--- utilFunctions/googleAnalytics/initGa.js
+++ utilFunctions/googleAnalytics/initGa.js
@@ -5,7 +5,6 @@ import ReactGA from "react-ga";
 // switching between pages of different types.
 const initGa = () => {
   if (!window.GA_INITIALIZED) {
-    alert("init ga");
     ReactGA.initialize(gaTrackingId);
     window.GA_INITIALIZED = true;
   }
