b0865c4a1432c2bf69f99d17933b1ffe10df2c49
Audrey Altman <audrey@dp.la>
2018-01-25 17:09:00 -0500

    works but need to remove _blank

--- components/shared/ListView/GaListViewWrapper.js
+++ components/shared/ListView/GaListViewWrapper.js
@@ -18,10 +18,11 @@ export default WrappedComponent =>
     // for Google Analytics tracking.
     componentDidMount() {
       initGa();
-      this.bindClickThroughEvent();
+      this.bindLinkEvent();
+      this.bindBrowseEvent();
     }
 
-    bindClickThroughEvent() {
+    bindLiknkEvent() {
       const links = document.getElementsByClassName("clickThrough");
       const items = this.props.items;
 
@@ -32,16 +33,55 @@ export default WrappedComponent =>
           return i.sourceUrl == link.href;
         })[0];
 
-        const gaEvent = {
-          type: "Click Through",
-          itemId: item.id,
-          title: item.title,
-          partner: item.provider,
-          contributor: item.dataProvider
-        };
+        // Sanity check
+        if (item) {
+          const gaEvent = {
+            type: "Click Through",
+            itemId: item.id,
+            title: joinIfArray(item.title),
+            partner: joinIfArray(item.provider),
+            contributor: joinIfArray(item.dataProvider)
+          };
 
-        bindLinkEvent(gaEvent, [link]);
+          bindLinkEvent(gaEvent, [link]);
+        }
       });
+    }
+
+    bindBrowseEvent() {
+      const path = window.location.pathname;
+      // Check if path starts with "/browse-by-partner" (backward compatible in IE).
+      // Need to check b/c component is shared between search and browse pages.
+      if (path.indexOf("/browse-by-topic") === 0) {
+        const links = document.getElementsByClassName("internalItemLink");
+        const items = this.props.items;
+
+        // alert(`${JSON.stringify(items[0].linkAs)} : ${JSON.stringify(links[0].href)}`);
+
+        // Iterate through links.
+        Array.from(links).forEach(function(link) {
+          // Find item with linkAs property that matches end of link href.
+          // item.linkAs is relative URL; link.href is full URL.
+          const item = items.filter(function(i) {
+            const suffix = i.linkAs;
+            const str = link.href;
+            return str.indexOf(suffix, str.length - suffix.length) !== -1;
+          })[0];
+
+          // Sanity check
+          if (item) {
+            const gaEvent = {
+              type: "Browse Item",
+              itemId: item.dplaItemId,
+              title: joinIfArray(item.title),
+              partner: joinIfArray(item.partner),
+              contributor: joinIfArray(item.dataProvider)
+            };
+
+            bindLinkEvent(gaEvent, [link]);
+          }
+        });
+      }
     }
 
     render() {
