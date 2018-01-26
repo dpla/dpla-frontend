64deb3e84bd94050fb502530cde247143e93f0c3
Mauricio Giraldo Arteaga <mauricio@pingpongestudio.com>
2018-01-26 14:49:23 -0500

    :space_invader: more hbreadcrumbs / minor typography

--- components/shared/FeatureHeader/index.js
+++ components/shared/FeatureHeader/index.js
@@ -6,8 +6,15 @@ import { classNames as utilClassNames } from "css/utils.css";
 const FeatureHeader = ({ title, description, titleClassName }) =>
   <div className={classNames.wrapper}>
     <div className={`${utilClassNames.container} ${classNames.content}`}>
-      <h1 className={`${titleClassName} ${classNames.text}`}>{title}</h1>
-      {description && <p className={classNames.description}>{description}</p>}
+      <h1
+        className={`${titleClassName ? titleClassName : ""} ${description === ""
+          ? classNames.long
+          : ""} ${classNames.text}`}
+      >
+        {title}
+      </h1>
+      {description !== "" &&
+        <p className={classNames.description}>{description}</p>}
     </div>
     <style dangerouslySetInnerHTML={{ __html: stylesheet }} />
   </div>;
