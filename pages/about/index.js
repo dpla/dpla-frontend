8b6d7dc6f32cc8f3a3c7a9462d1e54ac8983a0dd
Mauricio Giraldo Arteaga <mauricio@pingpongestudio.com>
2018-01-26 14:20:03 -0500

    :space_invader: similar breadcrumb template behavior in user site

--- pages/about/index.js
+++ pages/about/index.js
@@ -4,19 +4,28 @@ import Router from "next/router";
 
 import MainLayout from "components/MainLayout";
 import ContentPagesSidebar from "components/shared/ContentPagesSidebar";
-import {
-  classNames as contentClasses,
-  stylesheet as contentStyles
-} from "css/pages/content-pages-wysiwyg.css";
+import FeatureHeader from "shared/FeatureHeader";
+import BreadcrumbsModule from "shared/BreadcrumbsModule";
+
 import {
   ABOUT_MENU_ENDPOINT,
   GUIDES_ENDPOINT,
   SEO_TYPE
 } from "constants/content-pages";
+import { getBreadcrumbs, getItemWithId, getItemWithName } from "utilFunctions";
+
+import {
+  classNames as contentClasses,
+  stylesheet as contentStyles
+} from "css/pages/content-pages-wysiwyg.css";
 import { classNames as utilClassNames } from "css/utils.css";
 
-const AboutMenuPage = ({ url, content, items, pageTitle }) =>
+const AboutMenuPage = ({ url, content, items, breadcrumbs, pageTitle }) =>
   <MainLayout route={url} pageTitle={pageTitle} seoType={SEO_TYPE}>
+    {breadcrumbs.length > 0 &&
+      <BreadcrumbsModule breadcrumbs={breadcrumbs} route={url} />}
+    {breadcrumbs.length === 0 &&
+      <FeatureHeader title={pageTitle} description={""} />}
     <div
       className={`${utilClassNames.container}
       ${contentClasses.sidebarAndContentWrapper}`}
@@ -64,9 +73,44 @@ AboutMenuPage.getInitialProps = async ({ req, query, res }) => {
     }
     return {};
   }
+
+  // for the breadcrumbs
+  const breadcrumbObject = getBreadcrumbs({
+    items: json.items,
+    leafId: pageItem.object_id
+  });
+
+  let breadcrumbs = [];
+
+  if (JSON.stringify(breadcrumbObject) !== "{}") {
+    Object.values(breadcrumbObject).map(crumb => {
+      let slug = "/";
+      let url = "/about?section=" + crumb.post_name;
+      // if this is a child item the url is /:topsection/:thisitem
+      if (crumb.menu_item_parent !== "0") {
+        const parent = getItemWithId({
+          items: json.items,
+          id: crumb.menu_item_parent
+        });
+        slug = slug + parent.post_name + "/";
+      }
+      breadcrumbs.push({
+        title: crumb.title,
+        url: url,
+        as: slug + crumb.post_name
+      });
+    });
+    breadcrumbs.push({ title: pageItem.title });
+  }
+
   const pageRes = await fetch(pageItem.url);
   const pageJson = await pageRes.json();
-  return { content: pageJson, items: json.items, pageTitle: pageItem.title };
+  return {
+    content: pageJson,
+    items: json.items,
+    breadcrumbs: breadcrumbs,
+    pageTitle: pageItem.title
+  };
 };
 
 export default AboutMenuPage;
