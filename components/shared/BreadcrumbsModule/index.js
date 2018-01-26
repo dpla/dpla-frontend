d3f2edc1c9f7006348c81784db3e78fd074a7b67
Mauricio Giraldo Arteaga <mauricio@pingpongestudio.com>
2018-01-26 12:50:11 -0500

    :space_invader: 1 less api call in hub homepage / prod react in prod / universal breadcrumb start

---
+++ components/shared/BreadcrumbsModule/index.js
@@ -0,0 +1,23 @@
+import React from "react";
+import Link from "next/link";
+
+import Breadcrumbs from "components/Breadcrumbs";
+
+import { classNames as utilClassNames } from "css/utils.css";
+
+const { container } = utilClassNames;
+
+const BreadcrumbsModule = ({ route, breadcrumbs }) =>
+  <div className={utilClassNames.breadcrumbsWrapper}>
+    <div className={[container, utilClassNames.breadcrumbsModule].join(" ")}>
+      <Breadcrumbs
+        breadcrumbs={breadcrumbs.map(breadcrumb =>
+          Object.assign({}, breadcrumb, {
+            title: breadcrumb.title
+          })
+        )}
+      />
+    </div>
+  </div>;
+
+export default BreadcrumbsModule;
