e31aba7e1c3863c50a7d40b11869b83420b7f7e1
Mauricio Giraldo Arteaga <mauricio@pingpongestudio.com>
2018-01-26 14:02:18 -0500

    :space_invader: removed caching / improved pro wp template

--- utilFunctions/serverFunctions.js
+++ utilFunctions/serverFunctions.js
@@ -1,11 +1,4 @@
-const LRUCache = require("lru-cache");
-
-const ssrCache = new LRUCache({
-  max: 100,
-  maxAge: 1000 * 60 * 60 // 1hour
-});
-
-// route and cache related stuff
+// route related stuff
 
 exports.replaceWithProxyEndpoint = (endpoint, req) => {
   if (endpoint) {
@@ -19,14 +12,6 @@ exports.replaceWithProxyEndpoint = (endpoint, req) => {
   }
 };
 
-/*
-* NB: make sure to modify this to take into account anything that should trigger
-* an immediate page change (e.g a locale stored in req.session)
-*/
-exports.getCacheKey = req => {
-  return `${req.url}`;
-};
-
 exports.mergeQueryAndParams = (query, params) =>
   Object.assign({}, query, params);
 
@@ -38,27 +23,14 @@ exports.renderAndCache = (
   queryParams,
   extraParams
 ) => {
-  // const key = this.getCacheKey(req);
-
-  // If we have a page in the cache, let's serve it
-  // if (ssrCache.has(key)) {
-  //   console.log(`CACHE HIT: ${key}`);
-  //   res.send(ssrCache.get(key));
-  //   return;
-  // }
-
   if (extraParams) {
     queryParams = this.mergeQueryAndParams(extraParams, queryParams);
   }
 
-  // If not let's render the page into HTML
+  // let's render the page into HTML
   app
     .renderToHTML(req, res, pagePath, queryParams)
     .then(html => {
-      // Let's cache this page
-      // console.log(`CACHE MISS: ${key}`);
-      // ssrCache.set(key, html);
-
       res.send(html);
     })
     .catch(err => {
