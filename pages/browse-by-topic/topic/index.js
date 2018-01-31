4e229204b4d529ccbfce6e79323f8f929cec03ef
Mauricio Giraldo Arteaga <mauricio@pingpongestudio.com>
2018-01-31 14:15:26 -0500

    :space_invader: browse by topic using proper/custom wp api endpoints

--- pages/browse-by-topic/topic/index.js
+++ pages/browse-by-topic/topic/index.js
@@ -6,7 +6,10 @@ import BreadcrumbsAndNav from "components/TopicBrowseComponents/BreadcrumbsAndNa
 import MainContent from "components/TopicBrowseComponents/Topic/MainContent";
 import Suggestions from "components/TopicBrowseComponents/Topic/Suggestions";
 import MainLayout from "components/MainLayout";
-import { API_ENDPOINT_ALL_TOPICS_100_PER_PAGE } from "constants/topicBrowse";
+import {
+  API_ENDPOINT_ALL_TOPICS,
+  API_ENDPOINT_SUBTOPICS_FOR_TOPIC
+} from "constants/topicBrowse";
 import {
   EXHIBIT_PAGES_ENDPOINT,
   EXHIBITS_ENDPOINT,
@@ -46,15 +49,19 @@ const Topic = ({ url, topic, subtopics, suggestions }) =>
 
 Topic.getInitialProps = async ({ query, req }) => {
   const currentUrl = getCurrentUrl(req);
-  const topicsRes = await fetch(API_ENDPOINT_ALL_TOPICS_100_PER_PAGE);
+  const topicsRes = await fetch(
+    API_ENDPOINT_ALL_TOPICS + "?slug=" + query.topic
+  );
   const topicsJson = await topicsRes.json();
-  const currentTopic = topicsJson.find(topic => topic.slug === query.topic);
-  const subtopics = topicsJson
-    .filter(topic => topic.parent === currentTopic.id)
-    .sort((a, b) => a.acf.order - b.acf.order)
-    .map(subtopic =>
-      Object.assign({}, subtopic, { thumbnailUrl: subtopic.acf.category_image })
-    );
+  const currentTopic = topicsJson[0];
+
+  const subtopicsRes = await fetch(
+    API_ENDPOINT_SUBTOPICS_FOR_TOPIC + "?parent=" + currentTopic.term_id
+  );
+  const subtopicsJson = await subtopicsRes.json();
+  const subtopics = subtopicsJson.map(subtopic =>
+    Object.assign({}, subtopic, { thumbnailUrl: subtopic.acf.category_image })
+  );
 
   const suggestions = !currentTopic.acf.related_content
     ? []
