3ca7b9dcf79ed5c30988522e99b4373e4ff53d27
Mauricio Giraldo Arteaga <mauricio@pingpongestudio.com>
2018-02-01 18:17:26 -0500

    :space_invader: tag list in user site is different

--- components/NewsComponents/TagList/index.js
+++ components/NewsComponents/TagList/index.js
@@ -8,6 +8,7 @@ import {
   CONTENT_SHOWCASE_TAG_ID,
   NEWS_TAGS
 } from "constants/news.js";
+import { SITE_ENV } from "constants/env";
 
 import { classNames, stylesheet } from "./TagList.css";
 
@@ -21,21 +22,30 @@ const TagList = ({ url, currentTag }) =>
       </li>}
     {!currentTag && <li>All News</li>}
     {Object.keys(NEWS_TAGS).map(key => {
-      const item = Number(key) !== Number(currentTag)
-        ? <Link
-            prefetch
-            href={{
-              pathname: url.pathname,
-              query: Object.assign({}, url.query, {
-                tag: key,
-                page: 1
-              })
-            }}
-          >
-            <a>{NEWS_TAGS[key]}</a>
-          </Link>
-        : <span>{NEWS_TAGS[key]}</span>;
-      return <li key={key}>{item}</li>;
+      if (
+        (SITE_ENV === "user" &&
+          (Number(ANNOUNCEMENTS_TAG_ID) === key ||
+            Number(CONTENT_SHOWCASE_TAG_ID) === key)) ||
+        SITE_ENV === "pro"
+      ) {
+        const item = Number(key) !== Number(currentTag)
+          ? <Link
+              prefetch
+              href={{
+                pathname: url.pathname,
+                query: Object.assign({}, url.query, {
+                  tag: key,
+                  page: 1
+                })
+              }}
+            >
+              <a>{NEWS_TAGS[key]}</a>
+            </Link>
+          : <span>{NEWS_TAGS[key]}</span>;
+        return <li key={key}>{item}</li>;
+      } else {
+        return null;
+      }
     })}
     <style dangerouslySetInnerHTML={{ __html: stylesheet }} />
   </ul>;
