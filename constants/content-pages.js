d54ca23699fb059d0d07fea68bb69682d0076143
Mauricio Giraldo Arteaga <mauricio@pingpongestudio.com>
2018-02-01 15:34:40 -0500

    :space_invader: different news in user site

--- constants/content-pages.js
+++ constants/content-pages.js
@@ -1,11 +1,12 @@
 import { WORDPRESS_URL } from "./env";
+import { ANNOUNCEMENTS_TAG_ID, CONTENT_SHOWCASE_TAG_ID } from "./news.js";
 
 export const ABOUT_MENU_ENDPOINT = `${WORDPRESS_URL}/wp-json/menus/v1/menus/about-us`;
 export const PRO_MENU_ENDPOINT = `${WORDPRESS_URL}/wp-json/menus/v1/menus/pro-site`;
 export const NEWS_ENDPOINT = `${WORDPRESS_URL}/wp-json/wp/v2/posts`;
 export const PAGES_ENDPOINT = `${WORDPRESS_URL}/wp-json/wp/v2/pages`;
 export const GUIDES_ENDPOINT = `${PAGES_ENDPOINT}/2394`;
-export const NEWS_HOME_ENDPOINT = `${NEWS_ENDPOINT}?per_page=3`;
+export const NEWS_HOME_ENDPOINT = `${NEWS_ENDPOINT}?per_page=3&tags=${ANNOUNCEMENTS_TAG_ID},${CONTENT_SHOWCASE_TAG_ID}`;
 export const NEWS_HUB_ENDPOINT = `${NEWS_ENDPOINT}?per_page=3`;
 export const GUIDES_PARENT_ID = 5325; // the post id of “How Can I Use DPLA?” (which is different from the page id above)
 export const SEO_TYPE = "article";
--- constants/news.js
+++ constants/news.js
@@ -1,3 +1,5 @@
 export const TITLE = "DPLA News";
 export const DESCRIPTION =
   "Industry news, content announcements, press releases, and blog posts from our staff.";
+export const ANNOUNCEMENTS_TAG_ID = 151; // in WP: wp_terms.name == announcements
+export const CONTENT_SHOWCASE_TAG_ID = 268; // in WP: wp_terms.name == Content Showcase
