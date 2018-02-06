65c4db3b24cd18333970d97ed1914752d7046f4a
Mauricio Giraldo Arteaga <mauricio@pingpongestudio.com>
2018-02-06 10:40:54 -0500

    :space_invader: fixed news tagline

--- constants/news.js
+++ constants/news.js
@@ -1,6 +1,6 @@
 export const TITLE = "DPLA News";
 export const DESCRIPTION =
-  "Industry news, content announcements, press releases, and blog posts from our staff.";
+  "Announcements, project updates, and content highlights from our staff and community.";
 export const ANNOUNCEMENTS_TAG_ID = 151; // in WP: wp_terms.name == announcements
 export const CONTENT_SHOWCASE_TAG_ID = 268; // in WP: wp_terms.name == Content Showcase
 // from the WP DB wp_terms table:
