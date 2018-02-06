635244b2cbf1caf37e056943a8cd9d76913d1c8a
Mauricio Giraldo Arteaga <mauricio@pingpongestudio.com>
2018-02-06 15:17:07 -0500

    :space_invader: downgraded next / fixed citebuttons for new button component

--- components/PrimarySourceSetsComponents/SingleSet/SourceSetSources/index.js
+++ components/PrimarySourceSetsComponents/SingleSet/SourceSetSources/index.js
@@ -20,7 +20,7 @@ const SourceSetSources = ({ route, sources }) =>
       {sources.map(({ name, thumbnailUrl, useDefaultImage }, i) => {
         const sourceId = extractSourceId(sources[i]["@id"]);
         return (
-          <li className={classNames.set}>
+          <li className={classNames.set} key={i}>
             <Link
               key={name}
               prefetch
