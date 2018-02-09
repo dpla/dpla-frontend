b07ae262300765225ecc6a73757f180cab3135b7
Mauricio Giraldo Arteaga <mauricio@pingpongestudio.com>
2018-02-09 09:29:13 -0500

    :space_invader: added social media links / fixes #500

--- components/ContactComponents/MoreWaysToContact/index.js
+++ components/ContactComponents/MoreWaysToContact/index.js
@@ -24,7 +24,7 @@ const ContactForm = () =>
             617-859-2116
           </td>
         </tr>
-        <tr className="row">
+        <tr className={`row ${classNames.firstRow}`}>
           <td className={`col-xs-12 col-sm-5 ${classNames.tableHeading}`}>
             Mailing Address
           </td>
@@ -33,6 +33,53 @@ const ContactForm = () =>
             c/o Boston Public Library<br />
             700 Boylston Street<br />
             Boston, MA 02116<br />
+          </td>
+        </tr>
+        <tr className={`row ${classNames.firstRow}`}>
+          <td className={`col-xs-12 col-sm-5 ${classNames.tableHeading}`}>
+            Social Media
+          </td>
+          <td className={`col-xs-12 col-sm-7 ${classNames.tableItem}`}>
+            <ul>
+              <li><a href="//twitter.com/dpla">Twitter</a></li>
+              <li>
+                <a href="//www.facebook.com/digitalpubliclibraryofamerica">
+                  Facebook
+                </a>
+              </li>
+              <li><a href="//www.instagram.com/digpublib/">Instagram</a></li>
+              <li>
+                <a href="//digitalpubliclibraryofamerica.tumblr.com/">Tumblr</a>
+              </li>
+              <li><a href="//www.flickr.com/photos/dpla/albums">Flickr</a></li>
+              <li>
+                <a href="//www.youtube.com/user/digpublib/videos">YouTube</a>
+              </li>
+            </ul>
+          </td>
+        </tr>
+        <tr className={`row ${classNames.firstRow}`}>
+          <td className={`col-xs-12 col-sm-5 ${classNames.tableHeading}`}>
+            Mailing Lists
+          </td>
+          <td className={`col-xs-12 col-sm-7 ${classNames.tableItem}`}>
+            <ul>
+              <li>
+                <a href="https://us4.list-manage.com/subscribe?u=e1490d1305c4b651f3ad0ace4&id=1ee988aadc">
+                  General List
+                </a>
+              </li>
+              <li>
+                <a href="http://dp.us4.list-manage.com/subscribe?u=e1490d1305c4b651f3ad0ace4&id=de870d2705">
+                  Education List
+                </a>
+              </li>
+              <li>
+                <a href="http://dp.us4.list-manage2.com/subscribe?u=e1490d1305c4b651f3ad0ace4&id=f36418b975">
+                  Genealogy List
+                </a>
+              </li>
+            </ul>
           </td>
         </tr>
       </tbody>
