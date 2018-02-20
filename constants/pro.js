303986ebfc55c1f381f05a2b17072f46a530d696
Mauricio Giraldo Arteaga <mauricio@pingpongestudio.com>
2018-02-20 12:44:47 -0500

    :space_invader: prospective-hubs now top level

--- constants/pro.js
+++ constants/pro.js
@@ -1,5 +1,6 @@
 const SECTIONS = [
   { slug: "community-reps", name: "Community Reps" },
+  { slug: "prospective-hubs", name: "Prospective Hubs" },
   { slug: "about-dpla-pro", name: "About DPLA Pro" },
   { slug: "developers", name: "Developers" },
   { slug: "education", name: "Education" },
