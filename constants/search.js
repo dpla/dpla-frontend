f367d1a64362ca0f08e81f61c5d1c50b08dd1bab
Mauricio Giraldo Arteaga <mauricio@pingpongestudio.com>
2018-02-22 16:12:19 -0500

    :space_invader: starting with date facets

--- constants/search.js
+++ constants/search.js
@@ -2,6 +2,7 @@ export const possibleFacets = [
   "sourceResource.type",
   "sourceResource.subject.name",
   "sourceResource.spatial.name",
+  "sourceResource.date.begin",
   "sourceResource.language.name",
   "dataProvider",
   "provider.name"
@@ -11,6 +12,7 @@ export const mapFacetsToURLPrettified = {
   "sourceResource.type": "type",
   "sourceResource.subject.name": "subject",
   "sourceResource.spatial.name": "location",
+  "sourceResource.date.begin": "date",
   "sourceResource.language.name": "language",
   dataProvider: "provider",
   "provider.name": "partner"
@@ -20,9 +22,20 @@ export const mapURLPrettifiedFacetsToUgly = {
   type: "sourceResource.type",
   subject: "sourceResource.subject.name",
   location: "sourceResource.spatial.name",
+  date: "sourceResource.date.begin",
   language: "sourceResource.language.name",
   provider: "dataProvider",
   partner: "provider.name"
+};
+
+export const prettifiedFacetMap = {
+  "sourceResource.type": "Type",
+  "sourceResource.subject.name": "Subject",
+  "sourceResource.spatial.name": "Location",
+  "sourceResource.language.name": "Language",
+  "sourceResource.date.begin": "Date",
+  dataProvider: "Contributing Institution",
+  "provider.name": "Partner"
 };
 
 export const splitAndURIEncodeFacet = facet =>
