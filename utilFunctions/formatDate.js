fc159f67682edbaf90fb0726022a86fc67d972ba
Mauricio Giraldo Arteaga <mauricio@pingpongestudio.com>
2018-03-02 11:41:30 -0500

    :space_invader: removed intl from package, using polyfill.io

--- utilFunctions/formatDate.js
+++ utilFunctions/formatDate.js
@@ -1,6 +1,3 @@
-import Intl from "intl";
-import en from "intl/locale-data/jsonp/en";
-
 const formatDate = dateString => {
   const options = { month: "long", day: "numeric", year: "numeric" };
   const theDate = dateString ? new Date(dateString) : new Date();
