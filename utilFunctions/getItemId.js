367c40050f4cec8aec61aaa491160e75de716b2c
Mauricio Giraldo Arteaga <mauricio@pingpongestudio.com>
2018-03-06 12:06:51 -0500

    :space_invader: wrapping in try/finally in itemid

--- utilFunctions/getItemId.js
+++ utilFunctions/getItemId.js
@@ -2,13 +2,17 @@ import extractItemId from "./extractItemId";
 
 // Given source (i.e. JSON response from PSS API)
 // return item's DPLA ID
-const getItemId = source =>
-  source.mainEntity[0]["dct:references"]
-    ? extractItemId(
-        source.mainEntity[0]["dct:references"].filter(
-          ref => ref["@type"] === "ore:Aggregation"
-        )[0]["@id"]
-      )
-    : null;
+const getItemId = source => {
+  try {
+    const id = extractItemId(
+      source.mainEntity[0]["dct:references"].filter(
+        ref => ref["@type"] === "ore:Aggregation"
+      )[0]["@id"]
+    );
+    return id;
+  } finally {
+    return "";
+  }
+};
 
 export default getItemId;
