dac61f9658d91b30c1839aab257677e8eab36cfa
Mauricio Giraldo Arteaga <mauricio@pingpongestudio.com>
2018-03-06 12:05:17 -0500

    :space_invader: wrapping in try/finally

--- utilFunctions/getContributor.js
+++ utilFunctions/getContributor.js
@@ -1,11 +1,15 @@
 // Given a source (i.e. JSON response from primary source sets API)
 // return the contributor name.
 
-const getContributor = source =>
-  source.mainEntity[0]["provider"]
-    ? source.mainEntity[0]["provider"].filter(
-        ref => ref["disambiguationDescription"] == "contributing institution"
-      )[0]["name"]
-    : null;
+const getContributor = source => {
+  try {
+    const contributor = source.mainEntity[0]["provider"].filter(
+      ref => ref["disambiguationDescription"] == "contributing institution"
+    )[0]["name"];
+    return contributor;
+  } finally {
+    return "";
+  }
+};
 
 export default getContributor;
