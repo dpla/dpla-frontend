3d86480898c2e31e6e389da33a6f7cc44de3392d
Mauricio Giraldo Arteaga <mauricio@pingpongestudio.com>
2018-04-04 15:16:32 -0400

    :space_invader: trying pdfobject

--- components/shared/mediaViewers/PDFViewer.js
+++ components/shared/mediaViewers/PDFViewer.js
@@ -1,11 +1,32 @@
 import React from "react";
+import PropTypes from "prop-types";
+import PDFObject from "pdfobject";
 
-const PDFViewer = ({ pathToFile, height = "650px" }) =>
-  <iframe
-    title="dpla-pdf-viewer"
-    src={`https://docs.google.com/gview?url=${pathToFile}&embedded=true`}
-    style={{ width: "100%", height, display: "block" }}
-    frameBorder="0"
-  />;
+class PDFViewer extends React.Component {
+  componentDidMount() {
+    const { pathToFile, containerId } = this.props;
+
+    PDFObject.embed(pathToFile, `#${containerId}`);
+  }
+
+  render() {
+    const { pathToFile, width, height, containerId } = this.props;
+
+    return <div style={{ width, height }} id={containerId} />;
+  }
+}
+
+PDFViewer.propTypes = {
+  pathToFile: PropTypes.string.isRequired,
+  width: PropTypes.string,
+  height: PropTypes.string,
+  containerId: PropTypes.string
+};
+
+PDFViewer.defaultProps = {
+  width: "100%",
+  height: "650px",
+  containerId: "pdf-viewer"
+};
 
 export default PDFViewer;
