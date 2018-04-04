da03c74c0a8ff7df600e5f77eb7fef17aac94b3a
Mauricio Giraldo Arteaga <mauricio@pingpongestudio.com>
2018-04-04 10:51:55 -0400

    :space_invader: revisiting exhibitions landing page

--- components/ExhibitionsComponents/AllExhibitions/components/ExhibitionsList/index.js
+++ components/ExhibitionsComponents/AllExhibitions/components/ExhibitionsList/index.js
@@ -11,19 +11,18 @@ const Exhibition = ({ exhibition, route, featured }) => {
           pathname: "/exhibitions/exhibition",
           query: Object.assign({}, route.query, { exhibition: exhibition.slug })
         }}
-        as={{
-          pathname: `/exhibitions/${exhibition.slug}`,
-          query: route.query
-        }}
+        as={{ pathname: `/exhibitions/${exhibition.slug}`, query: route.query }}
       >
-        <a className={classNames.exhibition}>
+        <a>
           {featured &&
             <div className={classNames.featuredBanner}>
               Featured
             </div>}
           {exhibition.new &&
             !featured &&
-            <div className={classNames.newBanner}>New</div>}
+            <div className={classNames.newBanner}>
+              New
+            </div>}
           <div
             className={classNames.exhibitionContent}
             aria-labelledby={exhibition.slug}
@@ -33,7 +32,7 @@ const Exhibition = ({ exhibition, route, featured }) => {
               className={classNames.exhibitionImage}
               src={exhibition.thumbnailUrl}
             />
-            <div className={classNames.overlay}>
+            <div className={classNames.exhibitionText}>
               <span id={exhibition.slug} className={classNames.title}>
                 {exhibition.title}
               </span>
@@ -44,30 +43,20 @@ const Exhibition = ({ exhibition, route, featured }) => {
     : null;
 };
 
-const ThreeUp = ({ exhibitions, route }) =>
-  <div className={classNames.threeUp}>
-    <div className={classNames.firstExhibition}>
-      <Exhibition route={route} exhibition={exhibitions[0]} featured={true} />
-    </div>
-    <div className={classNames.twoVertical}>
-      <Exhibition route={route} exhibition={exhibitions[1]} />
-      <Exhibition route={route} exhibition={exhibitions[2]} />
-    </div>
-  </div>;
-
 const ExhibitionsList = ({ exhibitions, route }) =>
   <div className={`${classNames.wrapper} site-max-width`}>
     <div className={classNames.row}>
-      <div className={classNames.exhibitionList}>
+      <ul className={classNames.exhibitionList}>
         {exhibitions.map((exhibition, index) =>
-          <Exhibition
-            route={route}
-            exhibition={exhibition}
-            key={exhibition.slug}
-            featured={index > 0 ? false : true}
-          />
+          <li key={exhibition.slug} className={classNames.exhibition}>
+            <Exhibition
+              route={route}
+              exhibition={exhibition}
+              featured={index > 0 ? false : true}
+            />
+          </li>
         )}
-      </div>
+      </ul>
     </div>
     <style dangerouslySetInnerHTML={{ __html: stylesheet }} />
   </div>;
