24cca2a7b54c2ca9e1cd02b6820469c8970bf49a
Mauricio Giraldo Arteaga <mauricio@pingpongestudio.com>
2018-04-05 12:55:14 -0400

    :space_invader: started new layout

--- components/ExhibitionsComponents/AllExhibitions/components/ExhibitionsList/index.js
+++ components/ExhibitionsComponents/AllExhibitions/components/ExhibitionsList/index.js
@@ -3,7 +3,52 @@ import Link from "next/link";
 
 import { classNames, stylesheet } from "./ExhibitionsList.css";
 
-const Exhibition = ({ exhibition, route, featured }) => {
+const FeaturedExhibition = ({ exhibition, route }) => {
+  return exhibition
+    ? <div className={classNames.featuredExhibition}>
+        <div className={classNames.featuredBanner}>
+          Featured
+        </div>
+        <img
+          alt=""
+          className={classNames.exhibitionImage}
+          src={exhibition.thumbnailUrl}
+        />
+        <Link
+          prefetch
+          href={{
+            pathname: "/exhibitions/exhibition",
+            query: Object.assign({}, route.query, {
+              exhibition: exhibition.slug
+            })
+          }}
+          as={{
+            pathname: `/exhibitions/${exhibition.slug}`,
+            query: route.query
+          }}
+        >
+          <a className={`${classNames.exhibitionLink}`}>
+
+            <div
+              className={classNames.exhibitionContent}
+              aria-labelledby={exhibition.slug}
+            >
+
+              <h2 className={classNames.title}>
+                {exhibition.title}
+              </h2>
+              <div
+                className={classNames.title}
+                dangerouslySetInnerHTML={{ __html: exhibition.pageBlock.text }}
+              />
+            </div>
+          </a>
+        </Link>
+      </div>
+    : null;
+};
+
+const Exhibition = ({ exhibition, route }) => {
   return exhibition
     ? <Link
         prefetch
@@ -14,10 +59,6 @@ const Exhibition = ({ exhibition, route, featured }) => {
         as={{ pathname: `/exhibitions/${exhibition.slug}`, query: route.query }}
       >
         <a className={classNames.exhibitionLink}>
-          {featured &&
-            <div className={classNames.featuredBanner}>
-              Featured
-            </div>}
           {exhibition.new &&
             !featured &&
             <div className={classNames.newBanner}>
@@ -46,14 +87,16 @@ const Exhibition = ({ exhibition, route, featured }) => {
 const ExhibitionsList = ({ exhibitions, route }) =>
   <div className={`${classNames.wrapper} site-max-width`}>
     <div className={classNames.row}>
+      {JSON.stringify(exhibitions[0])}
+      <FeaturedExhibition
+        route={route}
+        exhibition={exhibitions[0]}
+        featured={true}
+      />
       <ul className={classNames.exhibitionList}>
-        {exhibitions.map((exhibition, index) =>
+        {exhibitions.slice(1).map((exhibition, index) =>
           <li key={exhibition.slug} className={classNames.exhibition}>
-            <Exhibition
-              route={route}
-              exhibition={exhibition}
-              featured={index > 0 ? false : true}
-            />
+            <Exhibition route={route} exhibition={exhibition} />
           </li>
         )}
       </ul>
