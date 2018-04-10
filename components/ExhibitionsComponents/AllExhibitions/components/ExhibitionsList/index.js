cb092bb796e098d80b32b3782c12ed50a2c19f49
Mauricio Giraldo Arteaga <mauricio@pingpongestudio.com>
2018-04-10 10:41:59 -0400

    :space_invader: no feature

--- components/ExhibitionsComponents/AllExhibitions/components/ExhibitionsList/index.js
+++ components/ExhibitionsComponents/AllExhibitions/components/ExhibitionsList/index.js
@@ -3,43 +3,6 @@ import Link from "next/link";
 
 import { classNames, stylesheet } from "./ExhibitionsList.css";
 
-const FeaturedExhibition = ({ exhibition, route }) => {
-  return exhibition
-    ? <Link
-        prefetch
-        href={{
-          pathname: "/exhibitions/exhibition",
-          query: Object.assign({}, route.query, {
-            exhibition: exhibition.slug
-          })
-        }}
-        as={{ pathname: `/exhibitions/${exhibition.slug}`, query: route.query }}
-      >
-        <a className={classNames.featuredExhibition}>
-          <div className={classNames.featuredBanner}>
-            Featured
-          </div>
-
-          <img
-            alt=""
-            className={classNames.exhibitionImage}
-            src={exhibition.thumbnailUrl}
-          />
-          <div className={classNames.exhibitionText}>
-            <a className={``}>
-              <h2 className={classNames.title}>
-                {exhibition.title}
-              </h2>
-            </a>
-            <p className={classNames.abstract}>
-              {exhibition.abstract}
-            </p>
-          </div>
-        </a>
-      </Link>
-    : null;
-};
-
 const Exhibition = ({ exhibition, route }) => {
   return exhibition
     ? <li>
@@ -82,21 +45,14 @@ const Exhibition = ({ exhibition, route }) => {
 const ExhibitionsList = ({ exhibitions, route }) =>
   <div className={`${classNames.wrapper} site-max-width`}>
     <div className={classNames.row}>
-      <FeaturedExhibition
-        route={route}
-        exhibition={exhibitions[0]}
-        featured={true}
-      />
       <ul className={classNames.exhibitionList}>
-        {exhibitions
-          .slice(1)
-          .map((exhibition, index) =>
-            <Exhibition
-              route={route}
-              exhibition={exhibition}
-              key={exhibition.slug}
-            />
-          )}
+        {exhibitions.map((exhibition, index) =>
+          <Exhibition
+            route={route}
+            exhibition={exhibition}
+            key={exhibition.slug}
+          />
+        )}
       </ul>
     </div>
     <style dangerouslySetInnerHTML={{ __html: stylesheet }} />
--- pages/exhibitions/index.js
+++ pages/exhibitions/index.js
@@ -34,15 +34,6 @@ Exhibitions.getInitialProps = async ({ req }) => {
           );
           const exhibitJson = await exhibitPageRes.json();
 
-          // get TOC for featured
-          const sectionInfo = exhibitJson
-            .filter(exhibition => !exhibition.parent)
-            .sort((a, b) => a.order - b.order);
-
-          const sections = sectionInfo.slice(1).map(section => section.title);
-
-          const abstract = `In this exhibition: ${sections.join(", ")}.`;
-
           const pageBlock = exhibitJson.find(
             exhibit =>
               exhibit.slug === "home-page" ||
@@ -58,8 +49,7 @@ Exhibitions.getInitialProps = async ({ req }) => {
 
           const thumbnailUrl = filesJson[0].file_urls.square_thumbnail;
           return Object.assign({}, exhibit, {
-            thumbnailUrl,
-            abstract
+            thumbnailUrl
           });
         })
         .reverse()
