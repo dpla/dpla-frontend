b3c8e7c92d5e8094d2b6681f9e390459b410ce1a
Mauricio Giraldo Arteaga <mauricio@pingpongestudio.com>
2018-04-13 10:16:28 -0400

    :space_invader: with next bundle analyzer

--- next.config.js
+++ next.config.js
@@ -1,44 +1,49 @@
 const webpack = require("webpack");
 const dotenv = require("dotenv").config();
-const { BundleAnalyzerPlugin } = require("webpack-bundle-analyzer");
-const { ANALYZE } = process.env;
 const withSass = require("@zeit/next-sass");
 const withSourceMaps = require("@zeit/next-source-maps");
+const withBundleAnalyzer = require("@zeit/next-bundle-analyzer");
+const { BundleAnalyzerPlugin } = require("webpack-bundle-analyzer");
 
-module.exports = withSourceMaps(
-  withSass({
-    cssModules: true,
-    cssLoaderOptions: {
-      importLoaders: 2,
-      localIdentName: "[name]__[local]___[hash:base64:5]"
-    },
-    useFileSystemPublicRoutes: false,
-    webpack: (config, options) => {
-      const { dev, isServer } = options;
-      if (ANALYZE) {
+module.exports = withBundleAnalyzer(
+  withSourceMaps(
+    withSass({
+      cssModules: true,
+      cssLoaderOptions: {
+        importLoaders: 2,
+        localIdentName: "[name]__[local]___[hash:base64:5]"
+      },
+      useFileSystemPublicRoutes: false,
+      analyzeServer: ["server", "both"].includes(process.env.BUNDLE_ANALYZE),
+      analyzeBrowser: ["browser", "both"].includes(process.env.BUNDLE_ANALYZE),
+      bundleAnalyzerConfig: {
+        server: {
+          analyzerMode: "static",
+          reportFilename: "../../bundles/server.html"
+        },
+        browser: {
+          analyzerMode: "static",
+          reportFilename: "../bundles/client.html"
+        }
+      },
+      webpack: (config, options) => {
+        const { dev, isServer } = options;
+
         config.plugins.push(
-          new BundleAnalyzerPlugin({
-            analyzerMode: "server",
-            analyzerPort: 8888,
-            openAnalyzer: true
+          new webpack.DefinePlugin({
+            "process.env": {
+              NODE_ENV: JSON.stringify(process.env.NODE_ENV)
+            }
           })
         );
+
+        config.module.rules.push({
+          test: /\.(jpe?g|png|gif|svg)$/i,
+          loaders: ["file-loader?name=dist/static/images/[name].[ext]"]
+        });
+
+        return config;
       }
-
-      config.plugins.push(
-        new webpack.DefinePlugin({
-          "process.env": {
-            NODE_ENV: JSON.stringify(process.env.NODE_ENV)
-          }
-        })
-      );
-
-      config.module.rules.push({
-        test: /\.(jpe?g|png|gif|svg)$/i,
-        loaders: ["file-loader?name=dist/static/images/[name].[ext]"]
-      });
-
-      return config;
-    }
-  })
+    })
+  )
 );
