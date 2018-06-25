70796102f8ae7d370d0ef447b6ca6f1d085f9bb3
Mauricio Giraldo Arteaga <mauricio@pingpongestudio.com>
2018-06-25 15:21:13 -0400

    :space_invader: added momentjs / started lists view

--- pages/lists/index.js
+++ pages/lists/index.js
@@ -1,60 +1,59 @@
 import React from "react";
+import moment from "moment";
 
 import MainLayout from "components/MainLayout";
 import FeatureHeader from "shared/FeatureHeader";
+
+import { getLocalForageLists } from "lib/localForage";
 
 import utils from "stylesheets/utils.scss";
 
 const TITLE = "Lists";
 const DESCRIPTION = "Your lists.";
 
-const Lists = ({ url }) =>
-  <MainLayout route={url} pageTitle={TITLE}>
-    <div id="main" role="main">
-      <FeatureHeader title={TITLE} description={DESCRIPTION} />
-      <div className={`${utils.container}`}>
-        <div className="">
-          <ul className="">
-            <li>one</li>
-            <li>two</li>
-            <li>three</li>
-          </ul>
-          <p>
-            Quia adipisci a cupiditate inventore. Fugit ratione quia cumque
-            ipsum. Aut porro occaecati id quasi. Et nemo dolores et et ut.
-          </p>
-          <p>
-            Placeat molestias veniam repudiandae quo quos non. Possimus incidunt
-            et quia perferendis rerum sint tenetur qui. Nesciunt laudantium
-            quasi doloribus. Corporis deserunt consequuntur in dolorum velit
-            repudiandae.
-          </p>
+class Lists extends React.Component {
+  state = { lists: [] };
 
-          <p>
-            Omnis architecto voluptas odio consequatur. Sit expedita error
-            dolorum qui non temporibus nihil. Sit maxime quia repellendus omnis
-            laudantium qui aut. Inventore voluptatem dolores est exercitationem.
-          </p>
+  componentDidMount() {
+    this.getLists();
+  }
 
-          <p>
-            Sed qui fugiat unde ut amet veritatis rerum voluptas. Consequatur
-            ducimus qui esse molestiae repellat sunt. Ut enim aliquid veritatis
-            molestiae est sapiente. Autem quaerat est commodi expedita
-            accusantium fugit culpa. Rerum quos nam beatae et est. Minus aperiam
-            illo minus eum ex animi.
-          </p>
-          <p>
-            Quisquam non dolores ex esse qui. Saepe hic ut illum magnam aperiam
-            repudiandae. Culpa eius est hic. Beatae explicabo quisquam et
-            similique quae. Quos accusamus laboriosam perspiciatis aspernatur
-            architecto eos repudiandae voluptatibus.
-          </p>
+  getLists = async () => {
+    let lists = await getLocalForageLists();
+    lists.sort((a, b) => a.createdAt > b.createdAt);
+    this.setState({
+      lists: lists
+    });
+  };
 
+  render() {
+    const { url } = this.props;
+    const { lists } = this.state;
+    return (
+      <MainLayout route={url} pageTitle={TITLE}>
+        <div id="main" role="main">
+          <FeatureHeader title={TITLE} description={DESCRIPTION} />
+          <div className={`${utils.container}`}>
+            <div className="">
+              <ul className="">
+                {lists.map((l, index) =>
+                  <li key={index}>
+                    <h2>
+                      {l.name}
+                    </h2>
+                    <span className="date">
+                      {moment(l.createdAt, "x").fromNow()}
+                    </span>
+                  </li>
+                )}
+              </ul>
+            </div>
+          </div>
         </div>
-      </div>
-    </div>
-  </MainLayout>;
-
+      </MainLayout>
+    );
+  }
+}
 Lists.getInitialProps = async ({ req }) => {
   return {};
 };
