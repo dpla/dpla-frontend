f623aa82d93db12553d42bed62ad9d0c2442c879
Mauricio Giraldo Arteaga <mauricio@pingpongestudio.com>
2018-07-06 14:27:00 -0400

    :space_invader: now saving works in ios

--- components/shared/ListView/index.js
+++ components/shared/ListView/index.js
@@ -225,17 +225,23 @@ class ListView extends React.Component {
       // IE 10+
       navigator.msSaveBlob(blob, filename);
     } else {
-      var link = document.createElement("a");
+      let link = document.createElement("a");
       if (link.download !== undefined) {
         // feature detection
         // Browsers that support HTML5 download attribute
-        var url = URL.createObjectURL(blob);
+        let url = URL.createObjectURL(blob);
         link.setAttribute("href", url);
         link.setAttribute("download", filename);
         link.style.visibility = "hidden";
         document.body.appendChild(link);
         link.click();
         document.body.removeChild(link);
+      } else {
+        let reader = new FileReader();
+        reader.onload = function(e) {
+          window.location.href = reader.result;
+        };
+        reader.readAsDataURL(blob);
       }
     }
   };
