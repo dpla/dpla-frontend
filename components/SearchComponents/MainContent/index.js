bd20c3e15f7e83962423769e03bb63908e189ee9
Mauricio Giraldo <mgiraldo@users.noreply.github.com>
2018-09-04 10:28:06 -0400

    added better empty states to search (#945)
    * :space_invader: added better empty states to search
    
    * :space_invader: removed unused constant
    
    * :space_invader: removed unnnecessary div

--- components/SearchComponents/MainContent/index.js
+++ components/SearchComponents/MainContent/index.js
@@ -4,33 +4,38 @@ import ListView from "shared/ListView";
 import Pagination from "shared/Pagination";
 import Sidebar from "./Sidebar";
 
-import { addLinkInfoToResults, removeQueryParams, extractItemId } from "lib";
+import { addLinkInfoToResults } from "lib";
 
 import utils from "stylesheets/utils.scss";
 import css from "./MainContent.scss";
 
 const MainContent = ({ results, route, facets, paginationInfo, hideSidebar }) =>
-  <div className={css.wrapper}>
-    <div className={[utils.container, css.mainContent].join(" ")}>
+  <div className={[utils.container, css.mainContent].join(" ")}>
+    {results.length > 0 &&
       <div className={`${!hideSidebar ? css.isOpen : ""} ${css.sidebar}`}>
         <Sidebar route={route} facets={facets} />
-      </div>
-      <div id="main" role="main" className={css.resultsAndPagination}>
+      </div>}
+    <div id="main" role="main" className={css.resultsAndPagination}>
+      {results.length > 0 &&
         <ListView
           route={route}
           items={addLinkInfoToResults(results, route.query)}
           viewMode={route.query.list_view}
-        />
-        <Pagination
-          route={route}
-          itemsPerPage={paginationInfo.pageSize}
-          currentPage={parseInt(paginationInfo.currentPage, 10)}
-          totalItems={paginationInfo.pageCount}
-          pageCount={Math.ceil(
-            paginationInfo.pageCount / paginationInfo.pageSize
-          )}
-        />
-      </div>
+        />}
+      {results.length === 0 &&
+        <div className={css.noResults}>
+          Your search did not match any items. Try different or more general
+          keywords or removing filters.
+        </div>}
+      <Pagination
+        route={route}
+        itemsPerPage={paginationInfo.pageSize}
+        currentPage={parseInt(paginationInfo.currentPage, 10)}
+        totalItems={paginationInfo.pageCount}
+        pageCount={Math.ceil(
+          paginationInfo.pageCount / paginationInfo.pageSize
+        )}
+      />
     </div>
   </div>;
 
