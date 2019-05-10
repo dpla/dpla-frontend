5bfd5c2a121648f7e51daf471a4ed9c157d1a02d
Michael Della Bitta <mdellabitta@gmail.com>
2019-05-10 09:24:06 -0400

    Initial implementation of Illinois local.

--- constants/local.js
+++ constants/local.js
@@ -14,6 +14,20 @@ const LOCALS = {
     hasTerms: false,
     hasContact: true
   },
+  illinois: {
+    theme: "illinois",
+    provider: "%22Illinois%20Digital%20Heritage%20Hub%22",
+    name: "Illinois Digital Heritage Hub",
+    favicon: "favicon.png",
+    logo: "logo.png",
+    description:
+      "The Illinois Digital Heritage Hub brings together unique digital cultural heritage resources from across Illinois. Explore collections immense in volume and rich in historical detail, including photographs, manuscripts, books, audio recordings, and videos. Established in 2015 by four institutions, the Chicago Public Library, the Consortium of Academic and Research Libraries of Illinois, the Illinois State Library, and the University of Illinois at Urbana-Champaign, the content of the IDHH is made possible by a network of more than one hundred contributors from across the state.",
+    locationFacet: "%22Illinois%22",
+    subjectFacet: "%22Illinois%22",
+    hasAbout: true,
+    hasTerms: false,
+    hasContact: false
+  },
   tennessee: {
     theme: "tennessee",
     provider: "%22Digital%20Library%20of%20Tennessee%22",
