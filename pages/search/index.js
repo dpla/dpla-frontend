61f3da222d82c75901383bf8597f23808f788c61
Michael Della Bitta <mdellabitta@gmail.com>
2020-01-28 11:30:52 -0500

    implementation of tags-based filtering in frontend.

--- pages/search/index.js
+++ pages/search/index.js
@@ -56,9 +56,9 @@ class Search extends React.Component {
       aboutness
     } = this.props;
 
-    var itemCount = 0 // default handles unexpected error
+    var itemCount = 0;// default handles unexpected error
     if ("count" in results) {
-      if (results.count.value != undefined) {
+      if (results.count.value !== undefined) {
         itemCount = results.count.value // ElasticSearch 7
       } else {
         itemCount = results.count // ElasticSearch 6
@@ -118,6 +118,13 @@ Search.getInitialProps = async context => {
           .replace(/"/g, "%22")
     : "";
 
+  let filters = isLocal && local.filters ? local.filters : [];
+  let tags = [];
+  if (query.tags) {
+    tags = Array.isArray(query.tags) ? query.tags : new Array(query.tags);
+    filters = filters.concat(tags.map(tag => `tags:${tag}`));
+  }
+
   let hasDates = false;
   const theseFacets = isQA ? qaFacets : possibleFacets;
 
@@ -155,8 +162,6 @@ Search.getInitialProps = async context => {
       return "";
     })
     .filter(facetQuery => facetQuery !== "");
-
-  let filters = isLocal && local.filters ? local.filters : [];
 
   const facetQueries = queryArray.join("&");
 
@@ -210,7 +215,7 @@ Search.getInitialProps = async context => {
       .filter(facet => facet && facet !== "+AND+" && facet !== "&").length;
 
     const facetsParam = `&facets=${theseFacets.join(",")}&${facetQueries}`;
-    const filtersParam = filters.map(x => `&filter=${x}`).join();
+    const filtersParam = filters.map(x => `&filter=${x}`).join("");
     const url = `${currentUrl}${API_ENDPOINT}?exact_field_match=true&q=${q}&page=${page}&page_size=${page_size}&sort_order=${sort_order}&sort_by=${sort_by}${facetsParam}${filtersParam}`;
 
     const res = await fetch(url);
@@ -233,6 +238,14 @@ Search.getInitialProps = async context => {
     theseFacets.forEach(facet => {
       if (json.facets[facet]) newFacets[facet] = json.facets[facet];
     });
+
+    if (tags) {
+      newFacets["tags"] = {
+        "_type" : "terms",
+        "terms" : tags
+      };
+    }
+
     json.facets = newFacets;
 
     const maxResults = MAX_PAGE_SIZE * page_size;
