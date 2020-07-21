89579c59d9cbfa2a20836087022afe94db9eec2c
Kiara Contreras <kcontreras01@gmail.com>
2020-07-21 14:47:19 -0400

    targeting env file for variables (#1065)

--- constants/env.example.js
+++ constants/env.example.js
@@ -1,7 +1,7 @@
 export const gaTrackingId = "UA-XXXXXXXX-X";
-export const LOCAL_ID = "PARTNER_ID";
-export const PRO_BASE_URL = "https://pro.example.org";
-export const PSS_BASE_URL = "https://example.org/api";
-export const SITE_ENV = "user";
-export const USER_BASE_URL = "https://example.org";
-export const WORDPRESS_URL = "https://wp.example.org";
+export const LOCAL_ID = process.env.LOCAL_ID;
+export const PRO_BASE_URL = process.env.PRO_BASE_URL;
+export const PSS_BASE_URL = process.env.PSS_BASE_URL;
+export const SITE_ENV = process.env.SITE_ENV;
+export const USER_BASE_URL = process.env.USER_BASE_URL;
+export const WORDPRESS_URL = process.env.WORDPRESS_URL;
