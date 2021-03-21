8f1b93b5bfe6466acb29d1422419b899095b0d89
Michael Della Bitta <mdellabitta@gmail.com>
2021-03-21 09:55:09 -0400

    current snapshots for top nav and homepage tests

--- snapshots.js
+++ snapshots.js
@@ -1,1 +1,235 @@
-{}
+module.exports = {
+  "The Home Page": {
+    "successfully loads": {
+      "Page title": "Digital Public Library of America"
+    },
+    "navigates to browse by topic": {
+      "Page title": "Browse by Topic | DPLA"
+    },
+    "navigates to browse by partner": {
+      "Page title": "Browse by Partner | DPLA"
+    },
+    "navigates to exhibitions": {
+      "Page title": "Exhibitions | DPLA"
+    },
+    "navigates to lists": {
+      "Page title": "My Lists | DPLA"
+    },
+    "navigates to about": {
+      "Page title": "About Us | DPLA"
+    },
+    "navigates to primary source sets": {
+      "Page title": "Primary Source Sets | DPLA"
+    }
+  },
+  "__version": "6.8.0",
+  "The top nav": {
+    "/'s browse-topics navigates to /browse-by-topic": {
+      "Page title": "Browse by Topic | DPLA"
+    },
+    "/'s browse-partners navigates to /browse-by-partner": {
+      "Page title": "Browse by Partner | DPLA"
+    },
+    "/'s my-lists navigates to /lists": {
+      "Page title": "My Lists | DPLA"
+    },
+    "/'s about-dpla navigates to /about": {
+      "Page title": "About Us | DPLA"
+    },
+    "/'s news navigates to /news": {
+      "Page title": "News | DPLA"
+    },
+    "/browse-by-topic's link-home navigates to /": {
+      "Page title": "Digital Public Library of America"
+    },
+    "/browse-by-topic's browse-topics navigates to /browse-by-topic": {
+      "Page title": "Browse by Topic | DPLA"
+    },
+    "/browse-by-topic's browse-partners navigates to /browse-by-partner": {
+      "Page title": "Browse by Partner | DPLA"
+    },
+    "/browse-by-topic's exhibitions navigates to /browse-by-exhibitions": {
+      "Page title": "Exhibitions | DPLA"
+    },
+    "/browse-by-topic's primary-source-sets navigates to /primary-source-sets": {
+      "Page title": "Primary Source Sets | DPLA"
+    },
+    "/browse-by-topic's my-lists navigates to /lists": {
+      "Page title": "My Lists | DPLA"
+    },
+    "/browse-by-topic's about-dpla navigates to /about": {
+      "Page title": "About Us | DPLA"
+    },
+    "/browse-by-topic's news navigates to /news": {
+      "Page title": "News | DPLA"
+    },
+    "/browse-by-partner's link-home navigates to /": {
+      "Page title": "Digital Public Library of America"
+    },
+    "/browse-by-partner's browse-topics navigates to /browse-by-topic": {
+      "Page title": "Browse by Topic | DPLA"
+    },
+    "/browse-by-partner's browse-partners navigates to /browse-by-partner": {
+      "Page title": "Browse by Partner | DPLA"
+    },
+    "/browse-by-partner's exhibitions navigates to /browse-by-exhibitions": {
+      "Page title": "Exhibitions | DPLA"
+    },
+    "/browse-by-partner's primary-source-sets navigates to /primary-source-sets": {
+      "Page title": "Primary Source Sets | DPLA"
+    },
+    "/browse-by-partner's my-lists navigates to /lists": {
+      "Page title": "My Lists | DPLA"
+    },
+    "/browse-by-partner's about-dpla navigates to /about": {
+      "Page title": "About Us | DPLA"
+    },
+    "/browse-by-partner's news navigates to /news": {
+      "Page title": "News | DPLA"
+    },
+    "/primary-source-sets's link-home navigates to /": {
+      "Page title": "Digital Public Library of America"
+    },
+    "/primary-source-sets's browse-topics navigates to /browse-by-topic": {
+      "Page title": "Browse by Topic | DPLA"
+    },
+    "/primary-source-sets's browse-partners navigates to /browse-by-partner": {
+      "Page title": "Browse by Partner | DPLA"
+    },
+    "/primary-source-sets's exhibitions navigates to /browse-by-exhibitions": {
+      "Page title": "Exhibitions | DPLA"
+    },
+    "/primary-source-sets's primary-source-sets navigates to /primary-source-sets": {
+      "Page title": "Primary Source Sets | DPLA"
+    },
+    "/primary-source-sets's my-lists navigates to /lists": {
+      "Page title": "My Lists | DPLA"
+    },
+    "/primary-source-sets's about-dpla navigates to /about": {
+      "Page title": "About Us | DPLA"
+    },
+    "/primary-source-sets's news navigates to /news": {
+      "Page title": "News | DPLA"
+    },
+    "/lists's link-home navigates to /": {
+      "Page title": "Digital Public Library of America"
+    },
+    "/lists's browse-topics navigates to /browse-by-topic": {
+      "Page title": "Browse by Topic | DPLA"
+    },
+    "/lists's browse-partners navigates to /browse-by-partner": {
+      "Page title": "Browse by Partner | DPLA"
+    },
+    "/lists's exhibitions navigates to /browse-by-exhibitions": {
+      "Page title": "Exhibitions | DPLA"
+    },
+    "/lists's primary-source-sets navigates to /primary-source-sets": {
+      "Page title": "Primary Source Sets | DPLA"
+    },
+    "/lists's my-lists navigates to /lists": {
+      "Page title": "My Lists | DPLA"
+    },
+    "/lists's about-dpla navigates to /about": {
+      "Page title": "About Us | DPLA"
+    },
+    "/lists's news navigates to /news": {
+      "Page title": "News | DPLA"
+    },
+    "/about's link-home navigates to /": {
+      "Page title": "Digital Public Library of America"
+    },
+    "/about's browse-topics navigates to /browse-by-topic": {
+      "Page title": "Browse by Topic | DPLA"
+    },
+    "/about's browse-partners navigates to /browse-by-partner": {
+      "Page title": "Browse by Partner | DPLA"
+    },
+    "/about's exhibitions navigates to /browse-by-exhibitions": {
+      "Page title": "Exhibitions | DPLA"
+    },
+    "/about's primary-source-sets navigates to /primary-source-sets": {
+      "Page title": "Primary Source Sets | DPLA"
+    },
+    "/about's my-lists navigates to /lists": {
+      "Page title": "My Lists | DPLA"
+    },
+    "/about's about-dpla navigates to /about": {
+      "Page title": "About Us | DPLA"
+    },
+    "/about's news navigates to /news": {
+      "Page title": "News | DPLA"
+    },
+    "/news's link-home navigates to /": {
+      "Page title": "Digital Public Library of America"
+    },
+    "/news's browse-topics navigates to /browse-by-topic": {
+      "Page title": "Browse by Topic | DPLA"
+    },
+    "/news's browse-partners navigates to /browse-by-partner": {
+      "Page title": "Browse by Partner | DPLA"
+    },
+    "/news's exhibitions navigates to /browse-by-exhibitions": {
+      "Page title": "Exhibitions | DPLA"
+    },
+    "/news's primary-source-sets navigates to /primary-source-sets": {
+      "Page title": "Primary Source Sets | DPLA"
+    },
+    "/news's my-lists navigates to /lists": {
+      "Page title": "My Lists | DPLA"
+    },
+    "/news's about-dpla navigates to /about": {
+      "Page title": "About Us | DPLA"
+    },
+    "/news's news navigates to /news": {
+      "Page title": "News | DPLA"
+    },
+    "/'s exhibitions navigates to /exhibitions": {
+      "Page title": "Exhibitions | DPLA"
+    },
+    "/'s primary-source-sets navigates to /primary-source-sets": {
+      "Page title": "Primary Source Sets | DPLA"
+    },
+    "/browse-by-topic's exhibitions navigates to /exhibitions": {
+      "Page title": "Exhibitions | DPLA"
+    },
+    "/browse-by-partner's exhibitions navigates to /exhibitions": {
+      "Page title": "Exhibitions | DPLA"
+    },
+    "/exhibitions's link-home navigates to /": {
+      "Page title": "Digital Public Library of America"
+    },
+    "/exhibitions's browse-topics navigates to /browse-by-topic": {
+      "Page title": "Browse by Topic | DPLA"
+    },
+    "/exhibitions's browse-partners navigates to /browse-by-partner": {
+      "Page title": "Browse by Partner | DPLA"
+    },
+    "/exhibitions's exhibitions navigates to /exhibitions": {
+      "Page title": "Exhibitions | DPLA"
+    },
+    "/exhibitions's primary-source-sets navigates to /primary-source-sets": {
+      "Page title": "Primary Source Sets | DPLA"
+    },
+    "/exhibitions's my-lists navigates to /lists": {
+      "Page title": "My Lists | DPLA"
+    },
+    "/exhibitions's about-dpla navigates to /about": {
+      "Page title": "About Us | DPLA"
+    },
+    "/exhibitions's news navigates to /news": {
+      "Page title": "News | DPLA"
+    },
+    "/primary-source-sets's exhibitions navigates to /exhibitions": {
+      "Page title": "Exhibitions | DPLA"
+    },
+    "/lists's exhibitions navigates to /exhibitions": {
+      "Page title": "Exhibitions | DPLA"
+    },
+    "/about's exhibitions navigates to /exhibitions": {
+      "Page title": "Exhibitions | DPLA"
+    },
+    "/news's exhibitions navigates to /exhibitions": {
+      "Page title": "Exhibitions | DPLA"
+    }
+  }
+}
