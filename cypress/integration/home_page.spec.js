98148817b3d1b9c9d4ac4447be32803fa96de9e8
Michael Della Bitta <mdellabitta@gmail.com>
2021-03-21 09:53:13 -0400

    test to prove content loads on home page

---
+++ cypress/integration/home_page.spec.js
@@ -0,0 +1,25 @@
+describe('The Home Page', () => {
+
+    beforeEach(() => {
+        cy.visit('/');
+      }
+    );
+
+    it('successfully loads', () => {
+        cy.checkTitle('Digital Public Library of America');
+        cy.contains('images, texts, videos, and sounds from across the United States');
+        cy.contains('Online Exhibitions');
+        cy.contains('Primary Source Sets');
+        cy.contains('How can I use DPLA?');
+        cy.contains('Education');
+        cy.contains('Family Research');
+        cy.contains('Lifelong Learning');
+        cy.contains('Scholarly Research');
+        cy.contains('View all user guides');
+        cy.contains('Get the latest DPLA news in your inbox');
+    });
+
+    it('doesn\'t have a link to home', () => {
+        cy.get('[data-cy=link-home]').should('not.exist')
+    }); 
+  });
