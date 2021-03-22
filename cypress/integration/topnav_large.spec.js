00a0849f1a1384e076c95c653e887c9d2ed660f9
Michael Della Bitta <mdellabitta@gmail.com>
2021-03-22 19:36:55 -0400

    split topnav spec into small and large

---
+++ cypress/integration/topnav_large.spec.js
@@ -0,0 +1,36 @@
+import pages from "../fixtures/pages.json";
+import topnav from "../fixtures/topnav.json";
+
+describe('The big screen top nav', () => {
+    
+    before(() => {
+        cy.viewport(1024, 768);
+    })
+
+    //big header
+    pages.forEach((origPage) => {
+        it(`hasn't changed on ${origPage}`, () => {
+            cy.visit(origPage);
+            cy.getDataCy('small-screen-header').should('not.be.visible');
+            cy.getDataCy('menu-button').should('not.be.visible');
+            const globalHeader = cy.getDataCy('global-header');
+            globalHeader.should('be.visible')
+            globalHeader.snapshot();
+
+        });
+        topnav.forEach((destPage) => {
+            // the home page doesn't have a home link, 
+            // so we have to skip home linking to home.
+            if (!(origPage === "/" && destPage.path === "/")) {
+                it(`on ${origPage}, ${destPage.selector} navigates to ${destPage.path}`, () => {
+                    cy.visit(origPage);
+                    cy.getDataCy('global-header').within(() => {
+                        cy.getDataCy(destPage.selector).click();
+                        cy.checkTitle(destPage.title);
+                    });
+                });
+            }
+        });        
+    });
+
+})
