0dd176737ab0b610cf1ba252960193d0453d4d48
Michael Della Bitta <mdellabitta@gmail.com>
2021-03-23 08:20:30 -0400

    squashed bugs

--- cypress/integration/bottomnav.spec.js
+++ cypress/integration/bottomnav.spec.js
@@ -12,9 +12,7 @@ describe('The bottom nav', () => {
 
         });
         ['DPLA', 'Tools'].forEach((section) => {
-            bottomnav['DPLA'].forEach((destPage) => {
-                // so we have to skip home linking to home.
-                // the home page doesn't have a home link, 
+            bottomnav[section].forEach((destPage) => {
                 it(`on ${origPage}, ${destPage.selector} navigates to ${destPage.path}`, () => {
                     cy.visit(origPage);
                     cy.getDataCy('footer').within(() => {
@@ -24,12 +22,14 @@ describe('The bottom nav', () => {
                 });
             });
         });
+
+        // The pro site stuff navigates off site, so we're just testing that the links
+        // have the correct href
         bottomnav['DPLA Pro'].forEach((destPage) => {
             it(`on ${origPage}, ${destPage.selector} links to ${destPage.url}`, () => {
                 cy.visit(origPage);
                 cy.getDataCy('footer').within(() => {
                     cy.getDataCy(destPage.selector).should('have.attr', 'href', destPage.url);
-                    cy.checkTitle(destPage.title);
                 });
             })
         })
