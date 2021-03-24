24e1fc6c21fe21804b81247582aba2ea9d125295
Michael Della Bitta <mdellabitta@gmail.com>
2021-03-23 21:37:30 -0400

    more tests for homepage

--- cypress/integration/home_page.spec.js
+++ cypress/integration/home_page.spec.js
@@ -20,7 +20,35 @@ describe('The Home Page', () => {
     });
 
     it('doesn\'t have a link to home', () => {
-        cy.get('[data-cy=link-home]').should('not.exist')
+        cy.getDataCy('link-home').should('not.exist')
     }); 
+
+    it('has the correct hero banner', () => {
+      cy.getDataCy('home-hero').within(() => {
+        cy.getDataCy('dpla-logo').snapshot();
+        cy.get('form').snapshot();
+      });
+    });
+
+    it('has guides', () => {
+      cy.getDataCy('guides').snapshot();
+    });
+
+    it('has the exhibitions slider', () => {
+      cy.getDataCy('Exhibitions').should('exist');
+    });
+
+    it('has the pss slider', () => {
+      cy.getDataCy("Sets").should('exist');
+    });
+
+    it('has news', () => {
+      const news = cy.getDataCy('news').should('exist')
+      news.get('.row').snapshot();
+    });
+
+    it('has stay informed', () => {
+      cy.getDataCy('stay-informed').snapshot();
+    });
 
   });
