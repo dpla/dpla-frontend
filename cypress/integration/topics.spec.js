d88b8d3bcd8cc1cf7d9ab4b9e26e7661abd80999
Michael Della Bitta <mdellabitta@gmail.com>
2021-03-28 11:51:30 -0400

    working tests for browse by topic

---
+++ cypress/integration/topics.spec.js
@@ -0,0 +1,29 @@
+const topics = require('../fixtures/topics.json')
+describe('Topics', async () => {
+
+    it(`home hasn't changed`, () => {
+        cy
+            .visit('/browse-by-topic')
+            .getDataCy('topics-home')
+            .snapshot();
+    });
+
+    for (const topic of topics) {
+        it(`${topic.slug}'s home hasn't changed`, async () => {
+            cy
+                .visit(`/browse-by-topic/${topic.slug}`)
+                .getDataCy('exhibition-home')
+                .snapshot();
+        });
+
+        for (const subtopic of topic.subtopics) {
+            it(`${topic.slug} ${subtopic} works correctly`, () => {
+                cy.visit(`/browse-by-topic/${topic.slug}/${subtopic}`);
+                cy.getDataCy('subtopic-sidebar').snapshot();
+                cy.getDataCy('subtopic-item-list').find('li').its('length').should('be.gt', 10);
+
+            });
+        }
+    }
+});
+
