636f4f348100d3327a10c7fc3097181d5b19789e
Michael Della Bitta <mdellabitta@gmail.com>
2021-04-05 20:04:24 -0400

    seadragon slain

--- cypress/integration/exhibitions.spec.js
+++ cypress/integration/exhibitions.spec.js
@@ -3,14 +3,14 @@ describe('Exhibitions', async () => {
 
     beforeEach(() => {
         cy.viewport(1024, 768);
-    });
+    })
 
     it(`home hasn't changed`, () => {
         cy
             .visit('/exhibitions')
             .getDataCy('exhibitions-home')
             .snapshot();
-    });
+    })
 
     for (const exhibit of exhibits) {
 
@@ -18,17 +18,25 @@ describe('Exhibitions', async () => {
             cy
                 .visit(`/exhibitions/${exhibit.slug}`)
                 .getDataCy('exhibition-home')
-                .snapshot();
-        });
+                .snapshot()
+        })
 
         for (const exhibitPage of exhibit.pages) {
             it(`${exhibit.slug}'s page ${exhibitPage} hasn't changed`, () => {
-                cy
-                    .visit(`/exhibitions/${exhibit.slug}/${exhibitPage}`)
-                    .getDataCy('exhibit-page')
-                    .snapshot();
-            });
+                cy.visit(`/exhibitions/${exhibit.slug}/${exhibitPage}`)
+                cy.window().then( window => {                    
+                    // seadragon's opacity and w/h params shift too much to work with 
+                    // snapshots so we're just going to take it out of the picture heh heh
+
+                    // todo switch to typescript to clean this kind of thing up with ?.
+                    const seadragon = window.document.getElementById("openseadragon1")
+                    if (seadragon) {
+                        seadragon.remove()
+                    }
+                    cy.getDataCy('exhibit-page').snapshot()
+                })
+            })
         }
     }
-});
+})
 
