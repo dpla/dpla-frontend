3df4ec2fa144af47e27935ee3aca1eba568436ca
Michael Della Bitta <michael@dp.la>
2021-10-09 11:21:30 -0400

    Cypress less pedantic (#1164)
    * stop testing every page for static content.
    
    * better
    
    * new snapshot baseline
    
    * testing about page because index doesn't have a link to home

--- cypress/integration/bottomnav.spec.js
+++ cypress/integration/bottomnav.spec.js
@@ -1,4 +1,3 @@
-import pages from "../fixtures/pages.json";
 import bottomnav from "../fixtures/bottomnav.json";
 
 
@@ -9,38 +8,34 @@ describe('The bottom nav', () => {
 
     beforeEach(() => {
         cy.viewport(1024, 768);
+        cy.visit('/')
     });
 
-    pages.forEach((origPage) => {
-        it(`hasn't changed on ${origPage}`, () => {
-            cy.visit(origPage);
-            const globalHeader = cy.getDataCy('footer');
-            globalHeader.should('be.visible')
-            globalHeader.snapshot();
+    it(`hasn't changed on /`, () => {
+        const globalHeader = cy.getDataCy('footer');
+        globalHeader.should('be.visible')
+        globalHeader.snapshot();
 
-        });
-        ['DPLA', 'Tools'].forEach((section) => {
-            bottomnav[section].forEach((destPage) => {
-                it(`on ${origPage}, ${destPage.selector} navigates to ${destPage.path}`, () => {
-                    cy.visit(origPage);
-                    cy.getDataCy('footer').within(() => {
-                        cy.getDataCy(destPage.selector).click();
-                        cy.checkTitle(destPage.title);
-                    });
+    });
+    ['DPLA', 'Tools'].forEach((section) => {
+        bottomnav[section].forEach((destPage) => {
+            it(`on /, ${destPage.selector} navigates to ${destPage.path}`, () => {
+                cy.getDataCy('footer').within(() => {
+                    cy.getDataCy(destPage.selector).click();
+                    cy.checkTitle(destPage.title);
                 });
             });
         });
-
-        // The pro site stuff navigates off site, so we're just testing that the links
-        // have the correct href
-        bottomnav['DPLA Pro'].forEach((destPage) => {
-            it(`on ${origPage}, ${destPage.selector} links to ${destPage.url}`, () => {
-                cy.visit(origPage);
-                cy.getDataCy('footer').within(() => {
-                    cy.getDataCy(destPage.selector).should('have.attr', 'href', destPage.url);
-                });
-            })
-        })
     });
+
+    // The pro site stuff navigates off site, so we're just testing that the links
+    // have the correct href
+    bottomnav['DPLA Pro'].forEach((destPage) => {
+        it(`on /, ${destPage.selector} links to ${destPage.url}`, () => {
+            cy.getDataCy('footer').within(() => {
+                cy.getDataCy(destPage.selector).should('have.attr', 'href', destPage.url);
+            });
+        })
+    })
 })
 
