c88538d51c5cd83fd239f184f7336c5da2d87cae
Michael Della Bitta <mdellabitta@gmail.com>
2022-02-02 16:45:13 -0500

    removed debug logging

--- pages/_document.js
+++ pages/_document.js
@@ -1,10 +1,7 @@
 import Document, { Html, Head, Main, NextScript } from "next/document";
 
-import { gaTrackingId } from "constants/env";
-
 export default class MyDocument extends Document {
   render() {
-      console.log(gaTrackingId);
     return (
       <Html lang={"en"} >
         <Head>
