0b67b72658b4b21bcc29ae37ceb2f91b39112a60
Michael Della Bitta <mdellabitta@tutanota.com>
2022-02-04 08:09:56 -0500

    added custom 404 page

---
+++ pages/404.js
@@ -0,0 +1,41 @@
+import utils from "stylesheets/utils.module.scss";
+import contentCss from "stylesheets/content-pages.module.scss";
+import donateCss from "stylesheets/donate.module.scss";
+import {SITE_ENV} from "constants/env";
+import ErrorLinksUser from "components/ErrorComponents/ErrorLinksUser";
+import ErrorLinksPro from "components/ErrorComponents/ErrorLinksPro";
+import MinimalLayout from "components/MainLayout/components/MinimalLayout";
+import React from "react";
+
+export default function Custom404() {
+    return (
+        <MinimalLayout>
+            <div
+                className={`${utils.container} ${contentCss.sidebarAndContentWrapper}`}
+            >
+                <div className="row">
+                    <div className={`col-md-2 col-xs-12`}/>
+                    <div
+                        id="main"
+                        role="main"
+                        className={`${contentCss.content} ${donateCss.thankYou} col-md-8 col-xs-12`}
+                    >
+                        <div>
+                            <h1>
+                                Page not found.
+                            </h1>
+                            <p>
+                                We are sorry that the page you requested cannot be found.
+                                Instead, try one of these:
+                            </p>
+                        </div>
+                        <div>
+                            {SITE_ENV === "user" && <ErrorLinksUser/>}
+                            {SITE_ENV === "pro" && <ErrorLinksPro/>}
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </MinimalLayout>
+    )
+}
