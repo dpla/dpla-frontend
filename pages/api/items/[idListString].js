d6999938e2bf6cdba0e5bbd87a2e45cdc15fe75d
Michael Della Bitta <mdellabitta@tutanota.com>
2022-02-11 08:17:09 -0500

    api to load a list's worth of items

---
+++ pages/api/items/[idListString].js
@@ -0,0 +1,49 @@
+import axios from "axios";
+
+const dplaIdRegex = /^[0-9a-f]{32}$/;
+
+export default async function handler(req, res) {
+
+    const { idListString } = req.query
+    const idList = idListString ? idListString.split(",") : []
+    const validIds = idList.filter(id => !!id && dplaIdRegex.test(id));
+
+    console.log("Loading", validIds);
+
+    if (validIds.length === 0) {
+        console.log("Zero valid ids");
+        res
+            .type("text/plain")
+            .status(404)
+            .end("Not found.");
+        return
+    }
+
+    try {
+        const url =
+            `http://api.dp.la/v2/items/${validIds.join(",")}` +
+            `?api_key=${process.env.API_KEY}`;
+
+        const axiosRes = await axios.get(url, {responseType: 'stream'});
+
+        if (axiosRes.status === 200) {
+            res
+                .type(axiosRes.headers['content-type'])
+                .status(200);
+            await axiosRes.data.pipe(res);
+
+        } else {
+            res
+                .type("text/plain")
+                .status(404)
+                .end("Not found.");
+        }
+
+    } catch (err) {
+        console.log("Error proxying request to DPLA API.", err);
+        res
+            .type("text/plain")
+            .status(404)
+            .end("Not found.");
+    }
+}
