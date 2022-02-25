5e7d85df8af0896aa363dbc8a25873b23126bafe
Michael Della Bitta <mdellabitta@gmail.com>
2022-02-25 11:00:12 -0500

    PPC Local Updates (#1204)
    - Changes links in navbar
    - Adds content and link to home

--- components/MainLayout/components/shared/NavigationLocal.js
+++ components/MainLayout/components/shared/NavigationLocal.js
@@ -1,163 +1,166 @@
-import React, { Component } from "react";
+import React, {Component} from "react";
 import Link from "next/link";
 
-import { LOCALS } from "constants/local";
-import { LOCAL_ID } from "constants/env";
+import {LOCALS} from "constants/local";
+import {LOCAL_ID} from "constants/env";
 
 class NavigationLocal extends Component {
 
-  render() {
-    const { isHome, className, css } = this.props;
-    let visitHtml;
-    let contactHtml;
-    let arbitraryHtml = null;
-    let blogHtml = null;
+    render() {
+        const {isHome, className, css} = this.props;
+        let visitHtml;
+        let contactHtml;
+        let arbitraryHtml = null;
+        let blogHtml = null;
 
-    if (LOCALS[LOCAL_ID].routes) {
-      const routesObj = LOCALS[LOCAL_ID].routes;
-      const dynamicRoutes = Object.keys(routesObj);
+        if (LOCALS[LOCAL_ID].routes) {
+            const routesObj = LOCALS[LOCAL_ID].routes;
+            const dynamicRoutes = Object.keys(routesObj);
 
-      const categories = dynamicRoutes
-          .map(function(category, i) {
-        const keys = Object.assign({}, i);
-        keys.id = i;
-        keys.route = dynamicRoutes[i];
-        keys.isTopLevel = routesObj[dynamicRoutes[i]].isTopLevel;
-        keys.category = routesObj[dynamicRoutes[i]].category;
-        return keys;
+            const categories = dynamicRoutes
+                .map(function (category, i) {
+                    const keys = Object.assign({}, i);
+                    keys.id = i;
+                    keys.route = dynamicRoutes[i];
+                    keys.isTopLevel = routesObj[dynamicRoutes[i]].isTopLevel;
+                    keys.category = routesObj[dynamicRoutes[i]].category;
+                    return keys;
 
-      }).filter(category =>
-        category.isTopLevel
-      );
+                }).filter(category =>
+                    category.isTopLevel
+                );
 
-      arbitraryHtml = categories.map(navItem => {
+            arbitraryHtml = categories.map(navItem => {
+                return (
+                    <li key={navItem.id}>
+                        <Link
+                            href={"/local" + navItem.route}
+                            as={navItem.route}>
+                            <a>{navItem.category}</a>
+                        </Link>
+                    </li>
+                );
+            });
+        }
+
+        if (LOCAL_ID === "aviation") {
+            visitHtml = (
+                <ul className={`${css.links} ${css.secondaryLinks}`}>
+                    <li>
+                        <a href={LOCALS[LOCAL_ID].externalLink}>PAN AM TIMELINE</a>
+                    </li>
+                </ul>
+            );
+            contactHtml = (
+                <li>
+                    <a href="https://dp.la/primary-source-sets/aviation">PAN AM PRIMARY SOURCE SET</a>
+                </li>
+            );
+
+        } else if (LOCAL_ID === "wisconsin") {
+
+            visitHtml = (
+                <ul className={`${css.links} ${css.secondaryLinks}`}>
+                    <li>
+                        <a href={LOCALS[LOCAL_ID].externalLink + "/explore/search"}>
+                            About
+                        </a>
+                    </li>
+                </ul>
+            );
+
+            contactHtml = (
+                <li>
+                    <a href={LOCALS[LOCAL_ID].externalLink + "/contact"}>Contact</a>
+                </li>
+            );
+
+        } else if (LOCAL_ID === "illinois") {
+            blogHtml = (
+                <li>
+                    <Link href={LOCALS[LOCAL_ID].externalLink}>
+                        <a>Highlights Blog</a>
+                    </Link>
+                </li>
+            );
+        } else if (LOCAL_ID === "plains2peaks") {
+            visitHtml = (
+                <ul className={`${css.links} ${css.secondaryLinks}`}>
+                    <li><a href={"https://ppc.cvlsites.org/"}>FOR CONTRIBUTORS</a></li>
+                    <li>
+                        <a href={"https://museumsfordigitallearning.org/museum/56/kits?page=2"}>
+                            FOR EDUCATORS AND STUDENTS
+                        </a>
+                    </li>
+                </ul>
+            );
+        } else if ("externalLink" in LOCALS[LOCAL_ID]) {
+            visitHtml = (
+                <ul className={`${css.links} ${css.secondaryLinks}`}>
+                    <li>
+                        <Link href={LOCALS[LOCAL_ID].externalLink}>
+                            <a href={LOCALS[LOCAL_ID].externalLink}>Visit {LOCALS[LOCAL_ID].name}</a>
+                        </Link>
+                    </li>
+                </ul>
+            );
+            contactHtml = null;
+
+        } else {
+            visitHtml = null;
+            contactHtml = null;
+        }
+
         return (
-          <li key={navItem.id}>
-            <Link
-              href={"/local" + navItem.route}
-              as={navItem.route}>
-              <a>{navItem.category}</a>
-            </Link>
-          </li>
+            <div className={className} id={"NavigationLocal"}>
+                <ul className={css.links}>
+                    {!isHome && (
+                        <li>
+                            <Link href="/local" as="/">
+                                <a>Home</a>
+                            </Link>
+                        </li>
+                    )}
+                    {arbitraryHtml}
+                    {contactHtml && contactHtml}
+                    {LOCALS[LOCAL_ID].hasTerms && (
+                        <li>
+                            <Link href="/terms">
+                                <a>Terms and Conditions</a>
+                            </Link>
+                        </li>
+                    )}
+                    {LOCALS[LOCAL_ID].hasBrowseByPartner && (
+                        <li>
+                            <Link href="/browse-by-partner">
+                                <a>Browse by Partner</a>
+                            </Link>
+                        </li>
+                    )}
+                    {LOCALS[LOCAL_ID].hasBrowseAll && (
+                        <li>
+                            <Link href="/search">
+                                <a>Browse All</a>
+                            </Link>
+                        </li>
+                    )}
+                    <li>
+                        <Link href="/lists">
+                            <a>My Lists</a>
+                        </Link>
+                    </li>
+                </ul>
+                {visitHtml && <span className={css.divider}/>}
+                {visitHtml && visitHtml}
+                <ul className={`${css.links} ${css.tertiaryLinks}`}>
+                    {blogHtml}
+                    <li>
+                        <a href="https://dp.la">Visit DPLA</a>
+                    </li>
+                </ul>
+            </div>
         );
-      });
     }
-
-    if (LOCAL_ID === "aviation") {
-      visitHtml = (
-        <ul className={`${css.links} ${css.secondaryLinks}`}>
-          <li>
-            <a href={LOCALS[LOCAL_ID].externalLink}>PAN AM TIMELINE</a>
-          </li>
-        </ul>
-      );
-      contactHtml = (
-        <li>
-          <a href="https://dp.la/primary-source-sets/aviation">PAN AM PRIMARY SOURCE SET</a>
-        </li>
-      );
-
-    } else if (LOCAL_ID === "wisconsin") {
-
-      visitHtml = (
-        <ul className={`${css.links} ${css.secondaryLinks}`}>
-          <li>
-            <a href={LOCALS[LOCAL_ID].externalLink + "/explore/search"}>
-              About
-            </a>
-          </li>
-        </ul>
-      );
-
-      contactHtml = (
-        <li>
-          <a href={LOCALS[LOCAL_ID].externalLink + "/contact"}>Contact</a>
-        </li>
-      );
-
-    } else if (LOCAL_ID === "illinois") {
-      blogHtml = (
-          <li>
-            <Link href={LOCALS[LOCAL_ID].externalLink}>
-              <a>Highlights Blog</a>
-            </Link>
-          </li>
-      );
-    } else if (LOCAL_ID === "plains2peaks") {
-      visitHtml = (
-        <ul className={`${css.links} ${css.secondaryLinks}`}>
-          <li>
-            <a href={"https://ppc.cvlsites.org/"}>Visit The Plains to Peaks Collective</a>
-          </li>
-        </ul>
-      );
-    } else if ("externalLink" in LOCALS[LOCAL_ID]) {
-      visitHtml = (
-        <ul className={`${css.links} ${css.secondaryLinks}`}>
-          <li>
-            <Link href={LOCALS[LOCAL_ID].externalLink}>
-              <a href={LOCALS[LOCAL_ID].externalLink}>Visit {LOCALS[LOCAL_ID].name}</a>
-            </Link>
-          </li>
-        </ul>
-      );
-      contactHtml = null;
-
-    } else {
-      visitHtml = null;
-      contactHtml = null;
-    }
-
-    return (
-      <div className={className} id={"NavigationLocal"}>
-        <ul className={css.links}>
-          {!isHome && (
-            <li>
-              <Link href="/local" as="/">
-                <a>Home</a>
-              </Link>
-            </li>
-          )}
-          {arbitraryHtml}
-          {contactHtml && contactHtml}
-          {LOCALS[LOCAL_ID].hasTerms && (
-            <li>
-              <Link href="/terms">
-                <a>Terms and Conditions</a>
-              </Link>
-            </li>
-          )}
-          {LOCALS[LOCAL_ID].hasBrowseByPartner && (
-            <li>
-              <Link href="/browse-by-partner">
-                <a>Browse by Partner</a>
-              </Link>
-            </li>
-          )}
-          {LOCALS[LOCAL_ID].hasBrowseAll && (
-            <li>
-              <Link href="/search">
-                <a>Browse All</a>
-              </Link>
-            </li>
-          )}
-          <li>
-            <Link href="/lists">
-              <a>My Lists</a>
-            </Link>
-          </li>
-        </ul>
-        {visitHtml && <span className={css.divider} />}
-        {visitHtml && visitHtml}
-        <ul className={`${css.links} ${css.tertiaryLinks}`}>
-          {blogHtml}
-          <li>
-              <a href="https://dp.la">Visit DPLA</a>
-          </li>
-        </ul>
-      </div>
-    );
-  }
 }
 
 export default NavigationLocal;
--- public/static/local/plains2peaks/homepage.md
+++ public/static/local/plains2peaks/homepage.md
@@ -1,43 +1,58 @@
 # Explore the Plains to Peaks
 
-Libraries, archives, and museums in Colorado and Wyoming all hold pieces of our rich cultural history. This online collection brings together these unique objects and their stories, allowing visitors to freely explore photographs, manuscripts, books, audio recordings, artifacts, and videos found at a variety of cultural heritage institutions in both states.
+Libraries, archives, and museums in Colorado and Wyoming all hold pieces of our rich cultural history. This online
+collection brings together these unique objects and their stories, allowing visitors to freely explore photographs,
+manuscripts, books, audio recordings, artifacts, and videos found at a variety of cultural heritage institutions in both
+states.
 <br> <br>
+
 ## Collection Highlights
 
 - ### [Mining](/search?q=mine*)
 
-  Explore how the rush to the Rockies for mining led to national and international technological advancements, drove the economy, and created numerous cities and towns, some of which survived and while others live on only as ghost towns.
+  Explore how the rush to the Rockies for mining led to national and international technological advancements, drove the
+  economy, and created numerous cities and towns, some of which survived and while others live on only as ghost towns.
 
 - ### [Railroads](/search?q=railway*+OR+railroad*)
 
-  Examine how huge feats of railroad engineering facilitated western expansion and settlement, and shaped the appearance and character of the Rocky Mountain West.
+  Examine how huge feats of railroad engineering facilitated western expansion and settlement, and shaped the appearance
+  and character of the Rocky Mountain West.
 
 - ### [Boosterism and Tourism](/search?q=camping+OR+Bike*+OR+Tourism+OR+Tourist+OR+Climbing+OR+Mountaineering+OR+raft*+OR+ski+OR+Skiing+OR+%22national+park%22)
 
-  Learn how boosterism played an important role in the initial exploration of these two states, paving the way for tourism to become one of the region's largest industries with several National Parks, a world-class ski industry and outdoor recreation destination that includes camping, mountaineering, endurance sports, and so much more.
+  Learn how boosterism played an important role in the initial exploration of these two states, paving the way for
+  tourism to become one of the region's largest industries with several National Parks, a world-class ski industry and
+  outdoor recreation destination that includes camping, mountaineering, endurance sports, and so much more.
 
 - ### [LGBTQ+](/search?q=Lesbian+OR+LGBT*+OR+bisexual)
 
-  Discover documents, photographs, oral histories, and artifacts that document the compelling and complex story of Colorado and Wyoming’s LGBTQ+ community.
+  Discover documents, photographs, oral histories, and artifacts that document the compelling and complex story of
+  Colorado and Wyoming’s LGBTQ+ community.
 
 - ### [Racial and ethnic minorities](/search?q=%22African+American%22+OR+%22Asian+American%22+OR+Native+American+OR+indigenous+OR+Chicano+OR+%22Mexican+American%22+OR+Chinese+OR+Hispanic+OR+Latino+OR+Migrant+OR+Jews+OR+Jewish+OR+Japanese+OR+Internment)
 
-  Learn from the lives and experiences of Black people, people of color, multiracial people, Native American or Indigenous peoples, Asian-Americans and Pacific Islanders, and immigrant or refugee populations who help shape our two states.
+  Learn from the lives and experiences of Black people, people of color, multiracial people, Native American or
+  Indigenous peoples, Asian-Americans and Pacific Islanders, and immigrant or refugee populations who help shape our two
+  states.
 
 - ### [Social Protest Movements](/search?q=%22Chicano+movement%22+OR+%22Civil+rights%22+OR+%22Women's+rights%22+OR+%22United+mine+worker*%22+OR+%22Ludlow+Strike%22+OR+Suffrag*+OR+%22Black+lives+matter%22+OR+%22Labor+movement%22+OR+%22Railroad+Strike*%22+OR+%22me+too+movement%22)
 
-  Explore how organized activism in Colorado and Wyoming impacted local and national movements such as women's suffrage, Black power, Chicanx/Latinx, voting rights, civil rights, women’s rights, labor rights, the Me Too movement, and Black Lives Matter.
+  Explore how organized activism in Colorado and Wyoming impacted local and national movements such as women's suffrage,
+  Black power, Chicanx/Latinx, voting rights, civil rights, women’s rights, labor rights, the Me Too movement, and Black
+  Lives Matter.
 
 - ### [Agriculture and Homesteading](/search?q=%22dry+farming%22+OR+farm*+OR+ranch*+OR+irrigation+OR+cowboy*+OR+stockmen+OR+homestead*+agriculture+OR+beet*+OR+crop*+OR+cattle+OR+sheep)
 
-  Examine how homesteading and agricultural activities such as dry farming, livestock production, cowboys and stockmen, irrigation, and water laws played an important part in the economic success of Colorado and Wyoming.
+  Examine how homesteading and agricultural activities such as dry farming, livestock production, cowboys and stockmen,
+  irrigation, and water laws played an important part in the economic success of Colorado and Wyoming.
 
 <br> <br>
 ## Collections by Format<br><br>
 
 - ### [Maps](/search?q=map&type=image)
 
-  Discover neighborhood, city, state, territory and topographic maps from 1846 to present day representing Colorado, Wyoming and the West.
+  Discover neighborhood, city, state, territory and topographic maps from 1846 to present day representing Colorado,
+  Wyoming and the West.
 
 - ### [Oral Histories](/search?q=oral+history&type=sound)
 
@@ -45,14 +60,26 @@ Libraries, archives, and museums in Colorado and Wyoming all hold pieces of our 
 
 - ### [Images](/search?type=image)
 
-  Explore thousands of images including photographs and postcards  depicting people, places and events.
+  Explore thousands of images including photographs and postcards depicting people, places and events.
 
 - ### [Physical Objects](/search?type=%22physical+object%22)
 
-  Discover a diverse set of objects including household and personal artifacts, clothing and textiles, agricultural and industrial materials, and archaeological collections that bring our history to life.
+  Discover a diverse set of objects including household and personal artifacts, clothing and textiles, agricultural and
+  industrial materials, and archaeological collections that bring our history to life.
+
+<br> <br>
+## [Resources for Students and Educations](https://museumsfordigitallearning.org/museum/56/kits?page=2)<br><br>
+
+PPC Resource Kits are student activities created with primary and secondary sources found at memory institutions in
+Colorado and Wyoming. The kits are designed to help students develop critical thinking while engaging in the history of
+our two states. Each kit includes different activities centered around a single topic and include explanatory
+information, a set of primary sources, and additional links to related sources and student analysis worksheets.
 
 <br><br>
+
 ## [PPC Digital Collections Blog](https://www.coloradovirtuallibrary.org/digital-colorado/)
 
-Colorado’s digital footprint is growing and there are many ways to get involved and historic collections available online. [Explore highlights and news](https://www.coloradovirtuallibrary.org/digital-colorado/) from digital collections and initiatives found throughout the state.
+Colorado’s digital footprint is growing and there are many ways to get involved and historic collections available
+online. [Explore highlights and news](https://www.coloradovirtuallibrary.org/digital-colorado/) from digital collections
+and initiatives found throughout the state.
 
