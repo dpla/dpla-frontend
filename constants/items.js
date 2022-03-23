1b8a56491ee0c6b7b96aedd1b38fd66613695d61
Michael Della Bitta <mdellabitta@gmail.com>
2022-03-23 14:36:29 -0400

    unused constant cleanup

--- constants/items.js
+++
@@ -1,4 +0,0 @@
-export const API_ENDPOINT = "/api/dpla/items";
-export const THUMBNAIL_ENDPOINT = "/thumb";
-export const LOCAL_ABOUT_ENDPOINT = "/api/dpla/local_about";
-export const RANDOM_ENDPOINT = "/api/dpla/random";
