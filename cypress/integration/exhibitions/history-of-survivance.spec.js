9fc2cda1192d1db1fb5495046b3958ab938c47f1
Michael Della Bitta <mdellabitta@tutanota.com>
2022-03-30 10:39:59 -0400

    breakout exhibition tests (#1210)

---
+++ cypress/integration/exhibitions/history-of-survivance.spec.js
@@ -0,0 +1,13 @@
+const exhibits = require('../../fixtures/exhibitions.json');
+const exhibit = exhibits["history-of-survivance"];
+describe(`Exhibition ${exhibit.slug}`, () => {
+        it(`${exhibit.slug}'s home hasn't changed`, () => {
+            cy.testExhibitHome(exhibit)
+        });
+        for (const page of exhibit.pages) {
+            it(`${exhibit.slug}'s page ${page} hasn't changed`, () => {
+                cy.testExhibitPage(exhibit, page);
+            });
+        }
+    }
+);
--- cypress/support/commands.js
+++ cypress/support/commands.js
@@ -8,3 +8,25 @@ Cypress.Commands.add('checkTitle', (title) => {
 Cypress.Commands.add('getDataCy', (selector) => {
   return cy.get(`[data-cy="${selector}"]`);
 })
+
+Cypress.Commands.add('testExhibitHome', (exhibit) => {
+    cy
+        .visit(`/exhibitions/${exhibit.slug}`)
+        .getDataCy('exhibition-home')
+        .snapshot();
+});
+
+Cypress.Commands.add('testExhibitPage', (exhibit, exhibitPage) => {
+    cy.visit(`/exhibitions/${exhibit.slug}/${exhibitPage}`)
+    cy.window().then( window => {
+        // seadragon's opacity and w/h params shift too much to work with
+        // snapshots so we're just going to take it out of the picture heh heh
+        const seadragon = window.document.getElementById("openseadragon1");
+
+        if (seadragon) {
+            seadragon.remove()
+        }
+    })
+    cy.get(`[data-cy="exhibit-page"]`).snapshot();
+});
+
