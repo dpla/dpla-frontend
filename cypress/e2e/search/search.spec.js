f1ad6eb4870d64c9a4b7fd65e6657adc38c24fc7
Michael Della Bitta <mdellabitta@tutanota.com>
2023-01-08 14:14:13 -0500

    rebase and refactor for exhibitions changes

--- cypress/e2e/search/search.spec.js
+++ cypress/e2e/search/search.spec.js
@@ -2,7 +2,6 @@ describe( 'Search',  () => {
     it('searches', async () => {
         cy.fixture('searches.json').then( (searches) => {
              searches.forEach(foo => {
-                 console.log(foo);
                  cy.visit(`/search?${foo}`);
              });
          });
