f40d48fc5c1ee001db517d53564c4d84fa7e7c21
Michael Della Bitta <mdellabitta@tutanota.com>
2024-11-02 11:26:52 -0400

    added sentry

--- pages/_error.js
+++ pages/_error.js
@@ -9,65 +9,62 @@ import { SITE_ENV } from "constants/env";
 
 import contentCss from "stylesheets/content-pages.module.scss";
 import donateCss from "stylesheets/donate.module.scss";
-import utils from "stylesheets/utils.module.scss"
+import utils from "stylesheets/utils.module.scss";
 
 class Error extends React.Component {
-
-    static getServerSideProps({ res, err }) {
-        const statusCode = res ? res.statusCode : err ? err.statusCode : null;
-        if (res) {
-            res.setHeader(
-                "Cache-Control",
-                "max-age=0, private, no-cache, no-store, must-revalidate"
-            );
-        }
-        return { statusCode };
+  static async getInitialProps({ res, err }) {
+    await Sentry.captureUnderscoreErrorException({ res, err });
+    const statusCode = res ? res.statusCode : err ? err.statusCode : null;
+    if (res) {
+      res.setHeader(
+        "Cache-Control",
+        "max-age=0, private, no-cache, no-store, must-revalidate"
+      );
     }
+    return { statusCode };
+  }
 
-    render() {
-        return (
-            <MinimalLayout>
-                <div
-                    className={`${utils.container} ${contentCss.sidebarAndContentWrapper}`}
-                >
-                    <div className="row">
-                        <div className={`col-md-2 col-xs-12`} />
-                        <div
-                            id="main"
-                            role="main"
-                            className={`${contentCss.content} ${donateCss.thankYou} col-md-8 col-xs-12`}
-                        >
-                            {this.props.statusCode !== 404 &&
-                            <div>
-                                <h1>
-                                    An error occurred.
-                                </h1>
-                                <p>
-                                    Thanks for your patience while we work to fix this issue.
-                                    In
-                                    the meantime, try one of these:
-                                </p>
-                            </div>}
-                            {this.props.statusCode === 404 &&
-                            <div>
-                                <h1>
-                                    Page not found.
-                                </h1>
-                                <p>
-                                    We are sorry that the page you requested cannot be found.
-                                    Instead, try one of these:
-                                </p>
-                            </div>}
-                            <div>
-                                {SITE_ENV === "user" && <ErrorLinksUser />}
-                                {SITE_ENV === "pro" && <ErrorLinksPro />}
-                            </div>
-                        </div>
-                    </div>
+  render() {
+    return (
+      <MinimalLayout>
+        <div
+          className={`${utils.container} ${contentCss.sidebarAndContentWrapper}`}
+        >
+          <div className="row">
+            <div className={`col-md-2 col-xs-12`} />
+            <div
+              id="main"
+              role="main"
+              className={`${contentCss.content} ${donateCss.thankYou} col-md-8 col-xs-12`}
+            >
+              {this.props.statusCode !== 404 && (
+                <div>
+                  <h1>An error occurred.</h1>
+                  <p>
+                    Thanks for your patience while we work to fix this issue. In
+                    the meantime, try one of these:
+                  </p>
                 </div>
-            </MinimalLayout>
-        );
-    }
+              )}
+              {this.props.statusCode === 404 && (
+                <div>
+                  <h1>Page not found.</h1>
+                  <p>
+                    We are sorry that the page you requested cannot be found.
+                    Instead, try one of these:
+                  </p>
+                </div>
+              )}
+              <div>
+                {SITE_ENV === "user" && <ErrorLinksUser />}
+                {SITE_ENV === "pro" && <ErrorLinksPro />}
+              </div>
+            </div>
+          </div>
+        </div>
+      </MinimalLayout>
+    );
+  }
 }
 
 export default withRouter(Error);
---
+++ sentry.client.config.js
@@ -0,0 +1,16 @@
+// This file configures the initialization of Sentry on the client.
+// The config you add here will be used whenever a users loads a page in their browser.
+// https://docs.sentry.io/platforms/javascript/guides/nextjs/
+
+import * as Sentry from "@sentry/nextjs";
+
+Sentry.init({
+  dsn:
+    "https://9a96f55c94d9e0f332251d903fbb60aa@o4508229092769792.ingest.us.sentry.io/4508229094080512",
+
+  // Define how likely traces are sampled. Adjust this value in production, or use tracesSampler for greater control.
+  tracesSampleRate: 1,
+
+  // Setting this option to true will print useful information to the console while you're setting up Sentry.
+  debug: false
+});
